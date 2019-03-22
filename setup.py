48fab02c82beb30787fe251fbda44a99e59d89ae
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-03-21 23:50:24 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,31 +9,38 @@ if sys.version_info [:2] < (3, 4):
     raise RuntimeError("Unsupported Python version")
 
 def readFile(filename):
-    with open(os.path.join(os.path.dirname(__file__), filename), 'r') as file:
+    with open(os.path.join(os.path.dirname(__file__), filename), 'r', encoding="utf-8") as file:
         return file.read()
-def readMe(filename="README.rst"):
+
+#https://docs.python.org/3/distutils/setupscript.html
+#https://packaging.python.org/tutorials/packaging-projects/#description
+#https://stackoverflow.com/questions/26737222/pypi-description-markdown-doesnt-work
+#https://stackoverflow.com/questions/1471994/what-is-setup-py
+def getReadMe(filename="README.md"):
     try:
-        return readFile(filename)
-    except FileNotFoundError as exception:
-        raise RuntimeError("File not found!")
-def requeriments(filename="requirements.txt"):
+        import pypandoc
+        return pypandoc.convert(filename, "rst").replace("\r","")
+    except(IOError, ImportError):
+        try:
+            return readFile(filename)
+        except FileNotFoundError as exception:
+            raise RuntimeError("File not found!")
+def getRequeriments(filename="requirements.txt"):
     try:
         return readFile(filename).splitlines()
     except FileNotFoundError as exception:
-        return [ "requests>=2.18.4", "requests-aeaweb>=0.0.1" ]
+        return [ "requests>=2.21.0", "requests-aeaweb>=0.0.1" ]
 def regexFunc(pattern):
     stringFile = readFile("pyrez/__init__.py")
     return Regex.search(r'^__{}__\s*=\s*[\'"]([^\'"]*)[\'"]'.format(pattern), stringFile, Regex.MULTILINE).group(1)
 
-VERSION = regexFunc("version")
-AUTHOR = regexFunc("author")
-LICENSE = regexFunc("license")
+NAME, AUTHOR, LICENSE, VERSION = regexFunc("name"), regexFunc("author"), regexFunc("license"), regexFunc("version")#https://www.python.org/dev/peps/pep-0440/
 
 setup(
     author=AUTHOR,
     author_email="luis.silva.1044894@gmail.com",
     classifiers=[#https://pypi.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: End Users/Desktop",
         "License :: OSI Approved :: MIT License",
@@ -46,23 +53,25 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Games/Entertainment",
         "Topic :: Internet",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries",
-        "Topic :: Software Development :: Libraries :: Python Modules"
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities"
     ],
     description="An open-source wrapper for Hi-Rez API (Paladins, Realm Royale, and Smite), written in Python",
     download_url="https://pypi.org/project/pyrez/#files",
     include_package_data=True,
-    install_requires=requeriments(),
-    keywords=["hirez hi-rez smite paladins realmapi open-source api wrapper library python api-wrapper paladins-api smitegame smiteapi realm-api python3 python-3 python-3-6"],
+    install_requires=getRequeriments(),
+    keywords=["pyrez hirez hi-rez smite paladins realmapi open-source api wrapper library python api-wrapper paladins-api smitegame smiteapi realm-api realm-royale python3 python-3 python-3-6"],
     license=LICENSE,
-    long_description=readMe(), # long_description=open ('README.rst').read () + '\n\n' + open ('HISTORY.rst').read (),
-    long_description_content_type="text/x-rst",
-    name="Pyrez",
-    packages=find_packages(), # packages=[name] # find_packages (exclude=['docs', 'tests*']),
+    long_description=getReadMe(), # long_description=open ('README.rst').read () + '\n\n' + open ('HISTORY.rst').read (),
+    long_description_content_type="text/markdown",#"text/x-rst", #https://guides.github.com/features/mastering-markdown/
+    name=NAME,
+    packages=find_packages(exclude=["docs", "tests", "examples", ".gitignore", ".gitattributes", "README.md"]),#find_packages(), # packages=[name] # find_packages (exclude=['docs', 'tests*']),
     url="https://github.com/luissilva1044894/pyrez",
     version=VERSION,
     zip_safe=True,
