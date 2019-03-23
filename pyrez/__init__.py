03697583dcd6665c96e0a27dcaeec71488bfc262
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-03-22 23:47:19 -0300

    Update __init__.py

--- pyrez/__init__.py
+++ pyrez/__init__.py
@@ -1,6 +1,11 @@
 
-__name__ = "Pyrez"
-__version__ = "0.9.8"
-__title__ = "{0}-{1}".format(__name__, __version__)
 __author__ = "Luis (Lugg) Gustavo"
+__author_email__ = "luis.silva.1044894@gmail.com"
+__copyright__ = "Copyright (c) 2018 Luís (Lugg) Gustavo"
+__build__ = 0x000908
+__description__ = "An open-source wrapper for Hi-Rez API (Paladins, Realm Royale, and Smite), written in Python"
 __license__ = "MIT"
+__name__ = "Pyrez"
+__title__ = "{0}-{1}".format(__name__, __version__)
+__url__ = "https://discord.gg/XkydRPS"
+__version__ = "0.9.8"
