147bf3f2a3037e1010de3769ceab32aa71dc5f39
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-03-25 15:53:02 -0300

    Removed trailing whitespace

--- pyrez/enumerations.py
+++ pyrez/enumerations.py
@@ -37,7 +37,7 @@ class Endpoint(BaseEnum):
     PALADINS = "http://api.paladins.com/paladinsapi.svc"
     REALM_ROYALE = "http://api.realmroyale.com/realmapi.svc"
     SMITE = "http://api.smitegame.com/smiteapi.svc"
-    
+
     HAND_OF_THE_GODS = "http://api.handofthegods.com/handofthegodsapi.svc"
     PALADINS_STRIKE = "http://api.paladinsstrike.com/paladinsstrike.svc"
 class Platform(BaseEnum):
