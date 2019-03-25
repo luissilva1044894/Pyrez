612c3c3a5d663b4c79533e4dd68d263365bcc602
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-03-25 15:42:41 -0300

    Removed explicit return on Exceptions' __init__()

--- pyrez/exceptions.py
+++ pyrez/exceptions.py
@@ -1,50 +1,50 @@
 class CustomException(Exception):
     def __init__(self, *args, **kwargs):
-        return super().__init__(self, *args, **kwargs)
+        super().__init__(self, *args, **kwargs)
     def __str__(self):
         return str(self.args[1]) if self.args is not None else "An error has occured within Pyrez"
 class DeprecatedException(CustomException):
     def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 class DailyLimitException(CustomException):
     def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 class InvalidArgumentException(CustomException):
     def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 class IdOrAuthEmptyException(CustomException):
     def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 class NotFoundException(CustomException):
     def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 class NotSupported(CustomException):
     def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 class SessionLimitException(CustomException):
     def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 class WrongCredentials(CustomException):
     def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 class PaladinsOnlyException(CustomException):
     def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 class SmiteOnlyException(CustomException):
     def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 class RealmRoyaleOnlyException(CustomException):
     def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 class PlayerNotFoundException(CustomException):
     def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 class GetMatchPlayerDetailsException(CustomException):
 	def __init__(self, *args, **kwargs):
-		return super().__init__(*args, **kwargs)
+		super().__init__(*args, **kwargs)
 class UnexpectedException(CustomException):
 	def __init__(self, *args, **kwargs):
-		return super().__init__(*args, **kwargs)
+		super().__init__(*args, **kwargs)
 class RequestErrorException(CustomException):
 	def __init__(self, *args, **kwargs):
-		return super().__init__(*args, **kwargs)
+		super().__init__(*args, **kwargs)
