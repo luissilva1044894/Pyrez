25cb710dcf913acba3d9a03cab89ed7fccb96a31
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-03-28 17:47:02 -0300

    Added `RealmRoyaleQueue` to getPlayerMatchDetails()

--- pyrez/models.py
+++ pyrez/models.py
@@ -524,20 +524,23 @@ class PlayerLoadout(APIResponse):
 class PlayerStatus(APIResponse):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.matchId = kwargs.get("Match", kwargs.get("match_id", 0)) if kwargs is not None else 0#currentMatchId
+        self.matchId = kwargs.get("Match", kwargs.get("match_id", 0)) if kwargs is not None else 0
         try:
             self.matchQueueId = PaladinsQueue(int(kwargs.get("match_queue_id")))
         except ValueError:
             try:
                 self.matchQueueId = SmiteQueue(int(kwargs.get("match_queue_id")))
             except ValueError:
-                self.matchQueueId = kwargs.get("match_queue_id", 0) if kwargs is not None else 0#Paladins only #currentMatchQueueId
+                try:
+                    self.matchQueueId = RealmRoyaleQueue(int(kwargs.get("match_queue_id")))
+                except ValueError:
+                    self.matchQueueId = kwargs.get("match_queue_id", 0) if kwargs is not None else 0
         try:
             self.status = Status(int(kwargs.get("status_id", kwargs.get("status"))))
         except ValueError:
-            self.status = kwargs.get("status_id", kwargs.get("status", 0)) if kwargs is not None else 0#playerStatusId
-        self.statusMessage = kwargs.get("personal_status_message", None) if kwargs is not None else None#playerStatusMessage
-        self.statusString = kwargs.get("status_string", kwargs.get("status", None)) if kwargs is not None else None#playerStatusString
+            self.status = kwargs.get("status_id", kwargs.get("status", 0)) if kwargs is not None else 0
+        self.statusMessage = kwargs.get("personal_status_message", None) if kwargs is not None else None
+        self.statusString = kwargs.get("status_string", kwargs.get("status", None)) if kwargs is not None else None
 class Session(APIResponse):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
