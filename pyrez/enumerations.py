1aa8617f0ad5133caef063826e48891b19d31b5f
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-03-29 10:20:36 -0300

    Update enumerations.py

--- pyrez/enumerations.py
+++ pyrez/enumerations.py
@@ -354,19 +354,19 @@ class SmiteQueue(BaseEnum):
     Novice_Queue = 424
     Conquest = 426
     Practice = 427
-    Conquest_Challenge = 429 #Conquest_Ranked = 430
+    Custom_Conquest = 429 #Conquest_Challenge=429#Conquest_Ranked=430
     Domination = 433
     MOTD = 434
     Arena_Queue = 435
     Basic_Tutorial = 436
-    Arena_Challenge = 438
+    Custom_Arena = 438#Arena_Challenge = 438
     Domination_Challenge = 439
     Joust_1v1_Ranked_Keyboard = 440
-    Joust_Challenge = 441
+    Custom_Joust = 441#Joust_Challenge = 441
     Arena_Practice_Easy = 443
     Jungle_Practice = 444
     Assault = 445
-    Assault_Challenge = 446
+    Custom_Assault = 446#Assault_Challenge = 446
     Joust_Queue_3v3 = 448
     Joust_3v3_Ranked_Keyboard = 450
     Conquest_Ranked_Keyboard = 451 # ConquestLeague
@@ -376,13 +376,13 @@ class SmiteQueue(BaseEnum):
     Arena_vs_AI_Easy = 457
     Conquest_Practice_Easy = 458
     Siege_4v4 = 459
-    Siege_Challenge = 460
+    Custom_Siege = 460#Siege_Challenge = 460
     Conquest_vs_AI_Medium = 461
     Arena_Tutorial = 462
     Conquest_Tutorial = 463
     Joust_Practice_Easy = 464
     Clash = 466
-    Clash_Challenge = 467
+    Custom_Clash = 467#Clash_Challenge = 467
     Arena_vs_AI_Medium = 468
     Clash_vs_AI_Medium = 469
     Clash_Practice_Easy = 470
