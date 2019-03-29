3380c58013421a589cd87499865855c888b032ef
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-03-29 08:19:43 -0300

    Update models.py

--- pyrez/models.py
+++ pyrez/models.py
@@ -21,7 +21,7 @@ class APIResponse(BaseAPIResponse):
 class AbstractPlayer(APIResponse):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.playerId = kwargs.get("Id", kwargs.get("id", 0)) if kwargs is not None else 0
+        self.playerId = kwargs.get("player_id", kwargs.get("Id", kwargs.get("id", 0))) if kwargs is not None else 0
         self.playerName = kwargs.get("Name", kwargs.get("name", None)) if kwargs is not None else None
 class MergedPlayer(APIResponse):
     def __init__(self, **kwargs):
