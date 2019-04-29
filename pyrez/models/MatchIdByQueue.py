26d15b52887dbd0bc244560b6baba6ed7eb150d7
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-04-29 01:00:26 -0300

    Update MatchIdByQueue.py

--- pyrez/models/MatchIdByQueue.py
+++ pyrez/models/MatchIdByQueue.py
@@ -2,5 +2,5 @@ from .APIResponse import APIResponse
 class MatchIdByQueue(APIResponse):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.matchId = kwargs.get("Match", 0) or kwargs.get("match", 0) if kwargs is not None else 0
+        self.matchId = kwargs.get("Match", kwargs.get("match", 0)) if kwargs is not None else 0
         self.activeFlag = str(kwargs.get("Active_Flag", kwargs.get("active_flag", None))).lower() == 'y' if kwargs is not None else False
