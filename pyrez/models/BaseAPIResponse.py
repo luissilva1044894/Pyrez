13ee7757bddfacb473cfaab88c4938c6636e1b10
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-02 10:12:51 -0300

    Update BaseAPIResponse.py

--- pyrez/models/BaseAPIResponse.py
+++ pyrez/models/BaseAPIResponse.py
@@ -3,11 +3,13 @@ class BaseAPIResponse(DictMixin):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.json = kwargs if kwargs else None
-    """def __getitem__(self, key):
+    """
+    def __getitem__(self, key):
         try:
             return self.json[key]
         except KeyError:
             return None
     def __str__(self):
         import json
-        return json.dumps(self.json) if self.json else None #return str(self.json).replace("'", "\"") if self.json else None"""
+        return json.dumps(self.json) if self.json else None
+    """
