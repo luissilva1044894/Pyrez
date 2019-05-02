0fdf4e9397fc249991412a209cf2420cb04ec1d5
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-02 10:00:19 -0300

    Create DictMixin.py

---
+++ pyrez/models/DictMixin.py
@@ -0,0 +1,11 @@
+class DictMixin:
+    def __init__(self, **kwargs):
+        self.__kwargs = kwargs if kwargs else None
+    def __getitem__(self, key):
+        try:
+            return self.__kwargs[key]
+        except KeyError:
+            return None
+    def __str__(self):
+        import json
+        return json.dumps(self.__kwargs) if self.__kwargs else None #return str(self.__kwargs).replace("'", "\"") if self.__kwargs else None
