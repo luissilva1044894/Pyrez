3202250aff9f709b7e8c8aea20ec68788547a28f
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-02 10:50:03 -0300

    Added Status Page endpoint

--- pyrez/enumerations/Endpoint.py
+++ pyrez/enumerations/Endpoint.py
@@ -7,6 +7,7 @@ class Endpoint(BaseEnum):
     REALM_ROYALE = "http://api.realmroyale.com/realmapi.svc"
     SMITE = "http://api.smitegame.com/smiteapi.svc"
     HIREZ = "https://api.hirezstudios.com"
+    STATUS_PAGE = "http://status.hirezstudios.com" #http://stk4xr7r1y0r.statuspage.io
 
     HAND_OF_THE_GODS = "http://api.handofthegods.com/handofthegodsapi.svc"
     PALADINS_STRIKE = "http://api.paladinsstrike.com/paladinsstrike.svc"
