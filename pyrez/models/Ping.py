b524e6e16a95649b8840aa44ecc769e47832ffad
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-02 02:06:44 -0300

    Update Ping.py

--- pyrez/models/Ping.py
+++ pyrez/models/Ping.py
@@ -9,6 +9,6 @@ class Ping:
             self.gamePatch = textPlain[5].replace(']', '')
             self.ping = textPlain[8] == "successful."
             #self.date = "{0} {1} {2}".format(textPlain [10].replace("Date:", ""), textPlain [11], textPlain [12])
-            self.date = datetime.strptime("{0} {1} {2}".format(textPlain [10].replace("Date:", ""), textPlain [11], textPlain [12]), "%m/%d/%Y %H:%M:%S %p")
+            self.date = datetime.strptime("{} {} {}".format(textPlain [10].replace("Date:", ""), textPlain [11], textPlain [12]), "%m/%d/%Y %H:%M:%S %p")
     def __str__(self):
-        return "APIName: {0} APIVersion: {1} GameVersion: {2} Ping: {3} Date: {4}".format(self.apiName, self.apiVersion, self.gamePatch, self.ping, self.date)
+        return "APIName: {} APIVersion: {} GameVersion: {} Ping: {} Date: {}".format(self.apiName, self.apiVersion, self.gamePatch, self.ping, self.date)
