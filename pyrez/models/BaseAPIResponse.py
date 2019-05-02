7fff321b7e20f239eba183d4d15601c981d69f2c
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-02 10:16:29 -0300

    Update BaseAPIResponse.py

--- pyrez/models/BaseAPIResponse.py
+++ pyrez/models/BaseAPIResponse.py
@@ -3,13 +3,3 @@ class BaseAPIResponse(DictMixin):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.json = kwargs if kwargs else None
-    """
-    def __getitem__(self, key):
-        try:
-            return self.json[key]
-        except KeyError:
-            return None
-    def __str__(self):
-        import json
-        return json.dumps(self.json) if self.json else None
-    """
