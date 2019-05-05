f3a7781b7ba7bb9801d15cdf39d58c71878a7ff5
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-05 11:41:24 -0300

    Update Player.py

--- pyrez/models/Player.py
+++ pyrez/models/Player.py
@@ -1,6 +1,7 @@
 from .APIResponse import APIResponse
 from pyrez.models.Mixin import Player as PlayerMixin
-class Player(PlayerMixin):
+class Player(APIResponse, PlayerMixin):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        APIResponse.__init__(self, **kwargs)
+        PlayerMixin.__init__(self, **kwargs)
         self.steamId = kwargs.get("steam_id", 0) if kwargs else 0
