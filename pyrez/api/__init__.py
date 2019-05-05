6f28980ac36736992feb6f8bbaa010ee2a813e2b
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-05 15:55:55 -0300

    Update __init__.py

--- pyrez/api/__init__.py
+++ pyrez/api/__init__.py
@@ -2,14 +2,14 @@
 #from .HandOfTheGods import HandOfTheGods
 #from .PaladinsStrike import PaladinsStrike
 
-#from .API import API
-#from .APIBase import APIBase
-#from .BaseSmitePaladins import BaseSmitePaladins
-#from .HiRez import HiRez
-#from .Paladins import Paladins
-#from .RealmRoyale import RealmRoyale
-#from .Smite import Smite
-#from .StatusPage import StatusPage
+from .API import API
+from .APIBase import APIBase
+from .BaseSmitePaladins import BaseSmitePaladins
+from .HiRez import HiRez
+from .Paladins import Paladins
+from .RealmRoyale import RealmRoyale
+from .Smite import Smite
+from .StatusPage import StatusPage
 #Cyclic import ^
 
 __all__ = [ "API", "APIBase", "BaseSmitePaladins", "HiRez", "Paladins", "RealmRoyale", "Smite", "StatusPage" ]#"HandOfTheGods", "PaladinsStrike"
