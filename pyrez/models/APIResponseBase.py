c539c0538f4085d95c1a184491599494dd19069f
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-09 01:35:01 -0300

    Update APIResponseBase.py

--- pyrez/models/APIResponseBase.py
+++ pyrez/models/APIResponseBase.py
@@ -2,4 +2,4 @@ from pyrez.models.Mixin import Dict
 class APIResponseBase(Dict):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.json = kwargs if kwargs else None
+        self.json = kwargs or []
