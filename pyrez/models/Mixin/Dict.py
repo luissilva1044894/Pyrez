5461e7a2c68393d253eea1da9bec39540421e0db
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-09 01:34:55 -0300

    Update Dict.py

--- pyrez/models/Mixin/Dict.py
+++ pyrez/models/Mixin/Dict.py
@@ -1,6 +1,6 @@
 class Dict:
     def __init__(self, **kwargs):
-        self.__kwargs = kwargs if kwargs else None
+        self.__kwargs = kwargs or []
     def __getitem__(self, key):
         try:
             return self.__kwargs[key]
@@ -8,4 +8,4 @@ class Dict:
             return None
     def __str__(self):
         import json
-        return json.dumps(self.__kwargs) if self.__kwargs else None #return str(self.__kwargs).replace("'", "\"") if self.__kwargs else None
+        return json.dumps(self.__kwargs, sort_keys=True, indent=2) if self.__kwargs else '' #return str(self.__kwargs).replace("'", "\"") if self.__kwargs else None
