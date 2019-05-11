3aabd3132accf403fc5e996d3d7d34f285530701
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-11 11:52:25 -0300

    Update __version__.py

--- pyrez/__version__.py
+++ pyrez/__version__.py
@@ -14,3 +14,14 @@ version = __version__
 
 from collections import namedtuple
 version_info = namedtuple("VersionInfo", "major minor micro releaselevel serial")(major=0, minor=9, micro=9, releaselevel="beta", serial=0)
+
+__all__ = (
+    "__title__",
+    "__description__",
+    "__url__",
+    "__version__",
+    "__author__",
+    #"__email__",
+    "__license__",
+    "__copyright__",
+)
