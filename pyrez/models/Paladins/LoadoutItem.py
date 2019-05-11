6b3b2ebf04c1e4c0517d7d17a72b44f20249b676
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-11 19:52:27 -0300

    Update LoadoutItem.py

--- pyrez/models/Paladins/LoadoutItem.py
+++ pyrez/models/Paladins/LoadoutItem.py
@@ -7,3 +7,7 @@ class LoadoutItem(APIResponseBase):
 		self.points = kwargs.get("Points", 0) if kwargs else 0
 	def __str__(self):
 		return "{0.itemName} ({0.points})".format(self)
+	def getCard(self):
+		return "https://web2.hirez.com/paladins/champion-cards/{}.jpg".format(self.itemName.lower().replace(' ', '-'))
+	def getFrame(self):
+		return "https://web2.hirez.com/paladins/cards/frame-{}.png".format(self.points)
