2c2350ba0194d855dcd41e6a5662d4ee6ae2c41c
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-12 11:56:03 -0300

    Update __init__.py

--- pyrez/__init__.py
+++ pyrez/__init__.py
@@ -1,7 +1,7 @@
 import sys
 from datetime import datetime
 if sys.version_info[:2] < (3, 4) and datetime.utcnow().year >= 2020:
-    raise RuntimeError("Unsupported Python version")
+    raise RuntimeError("Unsupported Python version - Pyrez requires Python 3.4+")
 
 from .api import *
 from .enumerations import *
