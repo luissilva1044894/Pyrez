43f933444caca89261ea062c957430e151fa1679
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-23 17:54:48 -0300

    Update MatchId.py

--- pyrez/models/Mixin/MatchId.py
+++ pyrez/models/Mixin/MatchId.py
@@ -1,5 +1,7 @@
 class MatchId:
     def __init__(self, **kwargs):
         self.matchId = kwargs.get("Match", kwargs.get("match", kwargs.get("match_id", 0))) if kwargs else 0
+    def __int__(self):
+    	return int(self.matchId) if str(self.matchId).isnumeric() else -1
     def __repr__(self):
         return "<MatchId {0.matchId}>".format(self)
