b25305e5862213933a6d2b869c8c8ea47685cabd
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-23 17:59:59 -0300

    Update APIResponse.py

--- pyrez/models/APIResponse.py
+++ pyrez/models/APIResponse.py
@@ -5,8 +5,8 @@ class APIResponse(APIResponseBase):
 	Keyword arguments/Parameters:
 		errorMsg [str]: The message returned from the API request.
 	"""
-    def __init__(self, **kwargs):
-    	super().__init__(**kwargs)
-    	self.errorMsg = kwargs.get("ret_msg", kwargs.get("error", kwargs.get("errors", None))) if kwargs else None
-    def hasError(self):
-        return self.errorMsg is not None
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.errorMsg = kwargs.get("ret_msg", kwargs.get("error", kwargs.get("errors", None))) if kwargs else None
+	def hasError(self):
+		return self.errorMsg is not None
