9b1972f420d81638ca24be15ee11639da7d7889a
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-23 17:44:53 -0300

    Update APIResponseBase.py

--- pyrez/models/APIResponseBase.py
+++ pyrez/models/APIResponseBase.py
@@ -1,5 +1,10 @@
 from pyrez.models.Mixin import Dict
 class APIResponseBase(Dict):
+	"""
+	The constructor for APIResponseBase class.
+	Keyword arguments/Parameters:
+		json [dict | list]: The request as JSON, if you prefer.
+	"""
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.json = kwargs or []
