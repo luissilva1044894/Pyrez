3945e8028d92299a755aefd437a734fa7c4027eb
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-24 01:25:47 -0300

    Update __init__.py

--- pyrez/__init__.py
+++ pyrez/__init__.py
@@ -1,3 +1,14 @@
+"""
+ _____
+|  __ \
+| |__) |   _ _ __ ___ ____
+|  ___/ | | | '__/ _ \_  /
+| |   | |_| | | |  __// /
+|_|    \__, |_|  \___/___|
+        __/ |
+       |___/
+"""
+
 import sys
 from datetime import datetime
 if sys.version_info[:2] < (3, 4) and datetime.utcnow().year >= 2020:
