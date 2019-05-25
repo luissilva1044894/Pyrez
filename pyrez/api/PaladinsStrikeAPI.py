e3e48a85de1ae32cb03f850742bfcac6bfb29855
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-25 16:56:47 -0300

    Update PaladinsStrikeAPI.py

--- pyrez/api/PaladinsStrikeAPI.py
+++ pyrez/api/PaladinsStrikeAPI.py
@@ -1,17 +1,5 @@
 from .API import API
 from pyrez.enumerations import Endpoint, Format
 class PaladinsStrikeAPI(API):
-    """
-    Class for handling connections and requests to Paladins Strike API.
-    """
     def __init__(self, devId, authKey, responseFormat=Format.JSON, sessionId=None, storeSession=True):
-        """
-        The constructor for PaladinsStrikeAPI class.
-        Keyword arguments/Parameters:
-            devId [int]: Used for authentication. This is the developer ID that you receive from Hi-Rez Studios.
-            authKey [str]: Used for authentication. This is the developer ID that you receive from Hi-Rez Studios.
-            responseFormat [pyrez.enumerations.Format]: The response format that will be used by default when making requests (default pyrez.enumerations.Format.JSON)
-            sessionId [str]: An active sessionId (default None)
-            storeSession [bool]: (default True)
-        """
         super().__init__(devId, authKey, Endpoint.PALADINS_STRIKE, responseFormat, sessionId, storeSession)
