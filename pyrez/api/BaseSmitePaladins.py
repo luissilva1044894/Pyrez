04f3d7c791ea658e5a71f6ef30ce0ee781c6a2e6
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-25 00:23:38 -0300

    Update BaseSmitePaladins.py

--- pyrez/api/BaseSmitePaladins.py
+++ pyrez/api/BaseSmitePaladins.py
@@ -26,6 +26,16 @@ class BaseSmitePaladins(API):
             Returns information regarding a particular match.  Rarely used in lieu of getmatchdetails().
         Keyword arguments/Parameters:
             matchId [int]:
+        
+        Raises
+        -------
+        pyrez.exceptions.DailyLimitException
+            Raised when the daily request limit is reached.
+        TypeError
+            Raised when an incorrect number of parameters is passed.
+        pyrez.exceptions.WrongCredentials
+            Raised when a wrong ``Credentials`` is passed.
+    
         """
         _ = self.makeRequest("getdemodetails", [matchId])
         if self._responseFormat.equal(Format.XML) or not _:
@@ -37,6 +47,16 @@ class BaseSmitePaladins(API):
         /getesportsproleaguedetails[ResponseFormat]/{devId}/{signature}/{session}/{timestamp}
             Returns the matchup information for each matchup for the current eSports Pro League season.
             An important return value is “match_status” which represents a match being scheduled (1), in-progress (2), or complete (3)
+        
+        Raises
+        -------
+        pyrez.exceptions.DailyLimitException
+            Raised when the daily request limit is reached.
+        TypeError
+            Raised when an incorrect number of parameters is passed.
+        pyrez.exceptions.WrongCredentials
+            Raised when a wrong ``Credentials`` is passed.
+    
         """
         _ = self.makeRequest("getesportsproleaguedetails")
         if self._responseFormat.equal(Format.XML) or not _:
@@ -50,6 +70,16 @@ class BaseSmitePaladins(API):
         Keyword arguments/Parameters:
             godId [int]:
             queueId [int]:
+        
+        Raises
+        -------
+        pyrez.exceptions.DailyLimitException
+            Raised when the daily request limit is reached.
+        TypeError
+            Raised when an incorrect number of parameters is passed.
+        pyrez.exceptions.WrongCredentials
+            Raised when a wrong ``Credentials`` is passed.
+    
         """
         _ = self.makeRequest("getgodleaderboard", [godId, queueId])
         if self._responseFormat.equal(Format.XML) or not _:
@@ -62,6 +92,16 @@ class BaseSmitePaladins(API):
             Returns the Rank and Worshippers value for each God a player has played.
         Keyword arguments/Parameters:
             playerId [int]:
+        
+        Raises
+        -------
+        pyrez.exceptions.DailyLimitException
+            Raised when the daily request limit is reached.
+        TypeError
+            Raised when an incorrect number of parameters is passed.
+        pyrez.exceptions.WrongCredentials
+            Raised when a wrong ``Credentials`` is passed.
+    
         Returns:
             List of pyrez.models.GodRank objects
         """
@@ -78,6 +118,16 @@ class BaseSmitePaladins(API):
             queueId [int]:
             tier [int]:
             split [int]:
+        
+        Raises
+        -------
+        pyrez.exceptions.DailyLimitException
+            Raised when the daily request limit is reached.
+        TypeError
+            Raised when an incorrect number of parameters is passed.
+        pyrez.exceptions.WrongCredentials
+            Raised when a wrong ``Credentials`` is passed.
+    
         """
         _ = self.makeRequest("getleagueleaderboard", [queueId, tier, split])
         if self._responseFormat.equal(Format.XML) or not _:
@@ -90,6 +140,16 @@ class BaseSmitePaladins(API):
             Provides a list of seasons (including the single active season) for a match queue.
         Keyword arguments/Parameters:
             queueId [int]:
+        
+        Raises
+        -------
+        pyrez.exceptions.DailyLimitException
+            Raised when the daily request limit is reached.
+        TypeError
+            Raised when an incorrect number of parameters is passed.
+        pyrez.exceptions.WrongCredentials
+            Raised when a wrong ``Credentials`` is passed.
+    
         """
         _ = self.makeRequest("getleagueseasons", [queueId])
         if self._responseFormat.equal(Format.XML) or not _:
