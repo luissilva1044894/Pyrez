9c48d9c76f5c1e447442d00cef58bbad3d6078e2
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-25 16:56:45 -0300

    Update HandOfTheGodsAPI.py

--- pyrez/api/HandOfTheGodsAPI.py
+++ pyrez/api/HandOfTheGodsAPI.py
@@ -1,17 +1,5 @@
 from .API import API
 from pyrez.enumerations import Endpoint, Format
 class HandOfTheGodsAPI(API):
-    """
-    Class for handling connections and requests to Hand of the Gods API.
-    """
     def __init__(self, devId, authKey, responseFormat=Format.JSON, sessionId=None, storeSession=True):
-        """
-        The constructor for HandOfTheGodsAPI class.
-        Keyword arguments/Parameters:
-            devId [int]: Used for authentication. This is the developer ID that you receive from Hi-Rez Studios.
-            authKey [str]: Used for authentication. This is the developer ID that you receive from Hi-Rez Studios.
-            responseFormat [pyrez.enumerations.Format]: The response format that will be used by default when making requests (default pyrez.enumerations.Format.JSON)
-            sessionId [str]: An active sessionId (default None)
-            storeSession [bool]: (default True)
-        """
         super().__init__(devId, authKey, Endpoint.HAND_OF_THE_GODS, responseFormat, sessionId, storeSession)
