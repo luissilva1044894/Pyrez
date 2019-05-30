978d782c876f592bf98fe167cdb521d4dff238bd
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-30 01:22:50 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -142,6 +142,7 @@ setup(
     packages=find_packages(exclude=["docs", "tests*", "examples", ".gitignore", ".github", ".gitattributes", "README.md"]),# packages=[name]
     platforms = "Any",
     python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,<4", #python_requires=">=3.0, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*, !=3.7.*, !=3.8.*",
+    requires=INSTALL_EXTRAS_REQUIRE,
     setup_requires=DEV_EXTRAS_REQUIRE,
     url=URL,
     version=VERSION,
