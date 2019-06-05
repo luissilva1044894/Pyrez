36978e8739b2df4dd302a7b606511f7559a69fa3
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-05 10:52:45 -0300

    Update Session.py

--- pyrez/models/Session.py
+++ pyrez/models/Session.py
@@ -4,7 +4,7 @@ class Session(APIResponse):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.sessionId = kwargs.get("session_id", None) if kwargs else None
-        self.timeStamp = kwargs.get("timestamp", None) if kwargs else None
+        self.timestamp = kwargs.get("timestamp", None) if kwargs else None
         if self.timestamp:
         	from ..utils import string_datetime_utc_to_datetime
         	self.timestamp = string_datetime_utc_to_datetime(self.timestamp)
