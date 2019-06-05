21ad9faba85e6aab9c02175f2f72f027fd065a82
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-05 10:43:26 -0300

    Update Session.py

--- pyrez/models/Session.py
+++ pyrez/models/Session.py
@@ -5,7 +5,8 @@ class Session(APIResponse):
         super().__init__(**kwargs)
         self.sessionId = kwargs.get("session_id", None) if kwargs else None
         self.timeStamp = kwargs.get("timestamp", None) if kwargs else None
-        if self.timeStamp:
-            self.timeStamp = datetime.strptime(self.timeStamp, "%m/%d/%Y %I:%M:%S %p")
+        if self.timestamp:
+        	from ..utils import from_iso_datetime
+        	self.timestamp = from_iso_datetime(self.timestamp)
     def isApproved(self):
         return self.errorMsg.lower().find("approved") != -1
