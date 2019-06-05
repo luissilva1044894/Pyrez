cf9e39b033ffa2dd175ba79f7ccd7cacef46e92d
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-05 10:51:42 -0300

    Update utils.py

--- pyrez/utils.py
+++ pyrez/utils.py
@@ -2,11 +2,9 @@
 # -*- coding: utf-8 -*-
 
 # ------- IMPORT DEPENDENCIES ------- 
-from time import time
+
 from datetime import datetime
-import json
 #from dateutil import *
-#from dateutil import tz
 # ------- IMPORT LOCAL DEPENDENCIES  -------
 
 def get_str():
@@ -42,8 +40,9 @@ def deprecated(instead=None):
         return decorated
     return actual_decorator
 
-def to_json(obj):
-    return json.dumps(obj, separators=(',', ':'), ensure_ascii=True)
+def to_json(obj, separators=(',', ':'), ensure_ascii=True):
+    import json
+    return json.dumps(obj, separators=separators, ensure_ascii=ensure_ascii)
 
 def retrieve_name(x):#, vars_=vars()):
     import inspect
@@ -60,7 +59,9 @@ def retrieve_name(x):#, vars_=vars()):
 # ------- DATETIME UTILS -------
 
 #def datetime_string_to_datetime_obj, *, strftime='%Y-%m-%dT%H:%M:%S', use_dateutil=True):#Convert datetime string to datetime obj with his format described in strftime argument function
-def from_iso_datetime(datetime_string, *, strftime='%m/%d/%Y %I:%M:%S %p'): #'%Y-%m-%dT%H:%M:%S', use_dateutil=True):
+def string_datetime_utc_to_datetime(datetime_string, strftime='%m/%d/%Y %I:%M:%S %p'):
+    return from_iso_datetime(datetime_string, strftime=strftime)
+def from_iso_datetime(datetime_string, *, strftime='%Y-%m-%dT%H:%M:%S', use_dateutil=True):
     """Parse an ISO8601-formatted datetime string and return a datetime object.
     Use dateutil's parser if possible and return a timezone-aware datetime.
     """
@@ -78,6 +79,7 @@ def datetime_obj_to_datetime_string(datetime_obj, strftime='%Y-%m-%d %H:%M:%S %H
 
 def datetime_local_to_datetime_utc(datetime_local):
     """Hardcode utc zone"""
+    from dateutil import tz
     utc_zone = tz.gettz('UTC')
     # utc_zone = tz.tzutc()# or Auto-detect utc zone
 
@@ -86,6 +88,7 @@ def datetime_local_to_datetime_utc(datetime_local):
 
 def datetime_utc_to_datetime_local(datetime_utc, local_zone = None):
     if local_zone is None :
+        from dateutil import tz
         # Hardcode local zone
         # local_zone = tz.gettz('America/Chicago')
         # or Auto-detect local zone
@@ -98,6 +101,7 @@ def string_timestamp_utc_to_string_datetime_utc(timestamp_utc, strftime = '%Y-%m
     return datetime.fromtimestamp(timestamp_utc).strftime(strftime)
 
 def string_datetime_utc_to_string_timestamp_utc(datetime_utc):
+    from time import time
     # timetuple() convert datetime obj to timestamp obj
     # time.mktime convert timestamp obj to timestamp string
     return time.mktime(datetime_utc.timetuple())
