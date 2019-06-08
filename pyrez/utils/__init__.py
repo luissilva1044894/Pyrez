1daf0060553351cccaac84433b295b4c4a381a38
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-08 10:44:50 -0300

    Update __init__.py

--- pyrez/utils/__init__.py
+++ pyrez/utils/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# ------- IMPORT DEPENDENCIES ------- 
+# ------- IMPORT DEPENDENCIES -------
 #import json
 
 # ------- IMPORT LOCAL DEPENDENCIES  -------
