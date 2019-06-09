c8f0bc82577af7d37230f72bf53abc9c1351dde4
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-09 17:55:49 -0300

    Update async.py

--- examples/async.py
+++ examples/async.py
@@ -1,7 +1,7 @@
 try:
     import asyncio
 except ImportError:
-    import trollius as asyncio #TODO: Expand the python < 3.3 example
+    import trollius as asyncio #TODO: Expand Python < 3.3 usage
 import pyrez
 
 devId=1004
@@ -9,7 +9,7 @@ authKey="23DF3C7E9BD14D84BF892AD206B6755C"
 
 async def default():
     paladins = pyrez.PaladinsAPI.Async(devId, authKey)
-    print (await paladins.getDataUsed())
+    print(await paladins.getDataUsed())
     paladins.close()
 
 async def context_manager():
