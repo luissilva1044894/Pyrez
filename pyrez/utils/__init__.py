d5ff31a4868878e7bf91ffcb09b7c10b49f16512
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-10 10:55:21 -0300

    Update __init__.py

--- pyrez/utils/__init__.py
+++ pyrez/utils/__init__.py
@@ -6,11 +6,13 @@
 
 # ------- IMPORT LOCAL DEPENDENCIES  -------
 
-def get_user_agent(dependencies):
+def get_user_agent(dependencies, origin=None):
     import sys
     from ..__version__ import __version__, __url__, __package_name__
-    __user_agent__ = '{pyrez} ({url} {ver}) [Python/{py.major}.{py.minor}.{py.micro} {dependencies.__name__}/{dependencies.__version__}]'
-    return __user_agent__.format(pyrez=__package_name__, url=__url__, ver=__version__, py=sys.version_info, dependencies=dependencies)
+    __user_agent__ = '{pyrez} ({url} {ver}) [Python/{py.major}.{py.minor}.{py.micro} {dependencies.__name__}/{dependencies.__version__}]'.format(pyrez=__package_name__, url=__url__, ver=__version__, py=sys.version_info, dependencies=dependencies)
+    if origin:
+        return { 'User-Agent': __user_agent__, 'Origin': origin }
+    return { 'User-Agent': __user_agent__ }
 
 def get_asyncio():
     try:
