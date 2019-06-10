7fd3911194df39ed81ceba082ed54dc14fdae46d
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-10 09:34:34 -0300

    Update __init__.py

--- pyrez/utils/__init__.py
+++ pyrez/utils/__init__.py
@@ -7,6 +7,7 @@
 # ------- IMPORT LOCAL DEPENDENCIES  -------
 
 def get_user_agent(dependencies):
+    import sys
     from ..__version__ import __version__, __url__, __package_name__
     __user_agent__ = '{pyrez} ({url} {ver}) [Python/{py.major}.{py.minor}.{py.micro} {dependencies.__name__}/{dependencies.__version__}]'
     return __user_agent__.format(pyrez=__package_name__, url=__url__, ver=__version__, py=sys.version_info, dependencies=dependencies)
