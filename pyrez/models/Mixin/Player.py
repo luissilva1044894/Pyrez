5cdef5d6b70fc01a66d5f6a88554f52d3c20185e
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-11 18:24:27 -0300

    Update Player.py

--- pyrez/models/Mixin/Player.py
+++ pyrez/models/Mixin/Player.py
@@ -1,7 +1,7 @@
 class Player:
     def __init__(self, **kwargs):
-        self.playerId = kwargs.get('player_id', kwargs.get('Id', kwargs.get('id', kwargs.get('playerId', 0)))) if kwargs else 0
-        self.playerName = kwargs.get('player_name', kwargs.get('Name', kwargs.get('name', kwargs.get('playerName', None)))) if kwargs else None
+        self.playerId = kwargs.get('player_id', kwargs.get('Id', kwargs.get('id', kwargs.get('playerId', 0)))) or 0
+        self.playerName = kwargs.get('player_name', kwargs.get('Name', kwargs.get('name', kwargs.get('playerName', None)))) or None
     def __repr__(self):
         return '<Player {0.playerName} ({0.playerId})>'.format(self)
     #def __hash__(self):
