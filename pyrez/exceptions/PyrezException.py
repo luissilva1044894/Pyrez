e59380b4bd15bbae086b7ffff856bccfbbba0c09
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-11 17:57:13 -0300

    Update PyrezException.py

--- pyrez/exceptions/PyrezException.py
+++ pyrez/exceptions/PyrezException.py
@@ -1,5 +1,8 @@
 class PyrezException(Exception):
-    """Base class for all other Pyrez exceptions."""
+    """Generic error class, catch-all for most Pyrez issues.
+
+    It's the base class for all other Pyrez exceptions.
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(self, *args, **kwargs)
     def __str__(self):
