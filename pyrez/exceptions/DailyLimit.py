22dc063281f78f0dd055f86566f8a52684398447
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-12 08:43:36 -0300

    Fix: IndentationError

--- pyrez/exceptions/DailyLimit.py
+++ pyrez/exceptions/DailyLimit.py
@@ -1,5 +1,5 @@
 from .PyrezException import PyrezException
 class DailyLimit(PyrezException):
 	"""Raised when you've hit a rate limit."""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
--- pyrez/exceptions/WrongCredentials.py
+++ pyrez/exceptions/WrongCredentials.py
@@ -1,5 +1,5 @@
 from .PyrezException import PyrezException
 class WrongCredentials(PyrezException):
 	"""Raised when you try to access a resource and it fails due to some issue with your authentication."""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
