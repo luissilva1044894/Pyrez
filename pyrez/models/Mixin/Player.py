b00ae0d01a7ceea843ff813af71966fd274312cc
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-12 19:37:44 -0300

    Update Player.py

--- pyrez/models/Mixin/Player.py
+++ pyrez/models/Mixin/Player.py
@@ -1,10 +1,17 @@
 class Player:
     def __init__(self, **kwargs):
-        self.playerId = kwargs.get('player_id', kwargs.get('Id', kwargs.get('id', kwargs.get('playerId', 0)))) or 0
-        self.playerName = kwargs.get('player_name', kwargs.get('Name', kwargs.get('name', kwargs.get('playerName', None)))) or None
+        self.playerId = kwargs.get('player_id', kwargs.get('Id', kwargs.get('id', kwargs.get('playerId', None)))) or 0
+        self.playerName = kwargs.get('player_name', kwargs.get('Name', kwargs.get('name', kwargs.get('playerName', None)))) or ''
     def __repr__(self):
         return '<Player {0.playerName} ({0.playerId})>'.format(self)
+    def __eq__(self, other) -> bool:
+        if isinstance(other, self.__class__):
+        	return self.id == other.id
+        return False
     #def __hash__(self):
         #return hash(self.playerId)
     def __eq__(self, other):
         return self.playerId == other.playerId
+    @property
+    def hidden_profile(self):
+    	return self.playerId == 0
