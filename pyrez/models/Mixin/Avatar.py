decd177cba9625bd9258512fd22db31f6275c1a1
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-12 19:46:57 -0300

    Update Avatar.py

--- pyrez/models/Mixin/Avatar.py
+++ pyrez/models/Mixin/Avatar.py
@@ -2,8 +2,8 @@ class Avatar:
     __slots__ = ('avatarId', 'avatarURL')
 
     def __init__(self, **kwargs):
-        self.avatarId = kwargs.get('avatarId',  kwargs.get('AvatarId', 0)) if kwargs else 0
-        self.avatarURL = kwargs.get('avatarURL', kwargs.get('Avatar_URL', None)) if kwargs else None
+        self.avatarId = kwargs.get('avatarId',  kwargs.get('AvatarId', 0)) or 0
+        self.avatarURL = kwargs.get('avatarURL', kwargs.get('Avatar_URL', None)) or None
     def __repr__(self):
         return '<Avatar {0.avatarId}>'.format(self)
     #def __hash__(self):
