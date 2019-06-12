5631b0df8db012bd13a4601b5116fc4757011f86
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-12 19:47:07 -0300

    Update Player.py

--- pyrez/models/Mixin/Player.py
+++ pyrez/models/Mixin/Player.py
@@ -1,7 +1,7 @@
 class Player:
     def __init__(self, **kwargs):
-        self.playerId = kwargs.get('player_id', kwargs.get('Id', kwargs.get('id', kwargs.get('playerId', None)))) or 0
-        self.playerName = kwargs.get('player_name', kwargs.get('Name', kwargs.get('name', kwargs.get('playerName', None)))) or ''
+        self.playerId = kwargs.get('player_id', kwargs.get('Id', kwargs.get('id', kwargs.get('playerId', 0)))) or 0
+        self.playerName = kwargs.get('player_name', kwargs.get('Name', kwargs.get('name', kwargs.get('playerName', '')))) or ''
     def __repr__(self):
         return '<Player {0.playerName} ({0.playerId})>'.format(self)
     def __eq__(self, other) -> bool:
