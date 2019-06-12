a905d473ec0fa2a3aeefb4ff6b369358b50c2311
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-12 09:49:12 -0300

    Update APIBase.py

--- pyrez/api/APIBase.py
+++ pyrez/api/APIBase.py
@@ -8,91 +8,15 @@ if ASYNC:
         #import aiofiles
     except ImportError:
         ASYNC = False#import trollius as asyncio; from trollius import From
-import requests
-class APIBase:
-    #Do not instantiate this object directly; instead, use::
-    """The constructor for APIBase class. DON'T INITALISE THIS YOURSELF!
-
-    Attributes
-    ----------
-    headers : |DICT|
-    cookies : |DICT|
-
-    Keyword Arguments
-    -----------------
-    headers : |DICT|
-    cookies : |DICT|
-
-    Methods
-    -------
-    __init__(devId, header=None)
-    _encode(string, encodeType="utf-8")
-    _httpRequest(url, headers=None)
-    """
-    def __init__(self, headers=None, cookies=None, raise_for_status=True, logger_name=None, debug_mode=True, is_async=False, loop=None):
-        from ..utils import get_user_agent#super().__init__(headers, cookies)
-        self._is_async = ASYNC
-        if ASYNC:
-            self._is_async = is_async
-            if self._is_async:
-                self.update_loop(loop)
-        self.debug_mode = debug_mode
-        if self.debug_mode:
-            from .. import logger#from ..__init__ import logger
-            self.logger = logger
-        self.headers = headers or get_user_agent(requests if not self._is_async else aiohttp)
-        self.cookies = cookies
-        self.__session__ = requests.Session() if not self._is_async else aiohttp.ClientSession(cookies=self.cookies, headers=self.headers, raise_for_status=raise_for_status)#loop=self.loop, connector=aiohttp.TCPConnector(limit=100),
-    #def __del__(self):
-    #    """Closes the session instance cleanly when the instance is deleted. Useful for things like when the interpreter closes."""
-    #    if ASYNC and self._is_async:
-    #        return self._close()
-    #    self.close()
-    def __enter__(self):
-        """We are our own iterator."""
-        return self
-    def __exit__(self, *args):
-        return self.close()
-    def _httpRequest(self, url, method="GET", raise_for_status=True, params=None, data=None, headers=None, cookies=None, json=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None, max_tries=3):
-        if ASYNC and self._is_async:
-            return self._async_httpRequest(url=url, method=method, params=params, data=data, headers=data, cookies=cookies, json=json, files=files, auth=auth, timeout=timeout, allowRedirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert, max_tries=max_tries)
-        from json.decoder import JSONDecodeError
-        with self.__session__.request(method=method, url=url.replace(' ', '%20'), params=params, json=json, data=data, headers=headers or self.headers, cookies=cookies or self.cookies, files=files, auth=auth, timeout=timeout, allow_redirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert) as resp:
-            self.cookies = resp.cookies
-            if raise_for_status:
-                resp.raise_for_status()#https://2.python-requests.org/en/master/api/#requests.Response.raise_for_status
-            try:
-                return resp.json()
-            except (JSONDecodeError, ValueError):
-                return resp.text
-    if ASYNC:
-        @classmethod
-        def Async(cls, headers=None, cookies=None, raise_for_status=True, logger_name=None, debug_mode=True, loop=None):
-            return cls(headers=headers, cookies=cookies, raise_for_status=raise_for_status, logger_name=loggerName, debug_mode=debug_mode, is_async=True, loop=loop)
-        @asyncio.coroutine
-        async def __aenter__(self):
-            """We are our own iterator."""
-            return self.__enter__()
-        async def __aexit__(self, *args):#, exc_type, exc, traceback
-            await self.close()#return
-        @staticmethod
-        def wrap(func):#https://github.com/Tinche/aiofiles/blob/master/aiofiles/os.py
-            from functools import partial, wraps
-            @asyncio.coroutine
-            @wraps(func)
-            def run(*args, loop=None, executor=None, **kwargs):
-                if not loop:
-                    loop = APIBaseMixin.get_loop()
-                    pfunc = partial(func, *args, **kwargs)
-                return loop.run_in_executor(executor, pfunc)
-            return run
-        async def __close(self):
-            # await self.__loop.close()
-            await self.__session__.close()
-        def close(self):
-            if self._is_async:
-                return self.__close()
-            self.__session__.close()
+    else: # Removes the aiohttp ClientSession instance warning.
+        class HTTPSession(aiohttp.ClientSession):#session = HTTPSession()
+            """Abstract class for aiohttp."""
+            def __init__(self, loop=None):
+                super().__init__(loop=loop or asyncio.get_event_loop())
+            def __del__(self):
+                """Closes the session instance cleanly when the instance is deleted. Useful for things like when the interpreter closes."""
+                if not self.closed:
+                    self.close()
         #https://www.tutorialsteacher.com/python/property-decorator
         @property
         def loop(self): return self.__loop
@@ -130,6 +54,73 @@ class APIBase:
         def update_loop(self, loop=None):
             self.__loop = loop or self.get_loop(True)
             asyncio.set_event_loop(self.__loop)
+import requests
+class APIBase:
+    #Do not instantiate this object directly; instead, use::
+    """The constructor for APIBase class. DON'T INITALISE THIS YOURSELF!
+
+    Attributes
+    ----------
+    headers : |DICT|
+    cookies : |DICT|
+
+    Keyword Arguments
+    -----------------
+    headers : |DICT|
+    cookies : |DICT|
+
+    Methods
+    -------
+    __init__(devId, header=None)
+    _encode(string, encodeType="utf-8")
+    _httpRequest(url, headers=None)
+    """
+    def __init__(self, headers=None, cookies=None, raise_for_status=True, logger_name=None, debug_mode=True, is_async=False, loop=None):
+        from ..utils import get_user_agent#super().__init__(headers, cookies)
+        self._is_async = ASYNC
+        if ASYNC:
+            self._is_async = is_async
+        self.debug_mode = debug_mode
+        if self.debug_mode:
+            from .. import logger#from ..__init__ import logger
+            self.logger = logger
+        self.headers = headers or get_user_agent(requests if not self._is_async else aiohttp)
+        self.cookies = cookies
+        self.__session__ = requests.Session() if not self._is_async else aiohttp.ClientSession(cookies=self.cookies, headers=self.headers, raise_for_status=raise_for_status)#loop=self.loop, connector=aiohttp.TCPConnector(limit=100),
+    def __enter__(self):
+        """We are our own iterator."""
+        return self
+    def __exit__(self, *args):
+        return self.close()
+    def _httpRequest(self, url, method="GET", raise_for_status=True, params=None, data=None, headers=None, cookies=None, json=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None, max_tries=3):
+        if ASYNC and self._is_async:
+            return self._async_httpRequest(url=url, method=method, params=params, data=data, headers=data, cookies=cookies, json=json, files=files, auth=auth, timeout=timeout, allowRedirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert, max_tries=max_tries)
+        from json.decoder import JSONDecodeError
+        with self.__session__.request(method=method, url=url.replace(' ', '%20'), params=params, json=json, data=data, headers=headers or self.headers, cookies=cookies or self.cookies, files=files, auth=auth, timeout=timeout, allow_redirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert) as resp:
+            self.cookies = resp.cookies
+            if raise_for_status:
+                resp.raise_for_status()#https://2.python-requests.org/en/master/api/#requests.Response.raise_for_status
+            try:
+                return resp.json()
+            except (JSONDecodeError, ValueError):
+                return resp.text
+    if ASYNC:
+        @classmethod
+        def Async(cls, headers=None, cookies=None, raise_for_status=True, logger_name=None, debug_mode=True, loop=None):
+            return cls(headers=headers, cookies=cookies, raise_for_status=raise_for_status, logger_name=loggerName, debug_mode=debug_mode, is_async=True, loop=loop)
+        async def __close(self):
+            # await self.__loop.close()
+            await self.__session__.close()
+        def close(self):
+            if self._is_async:
+                return self.__close()
+            self.__session__.close()
+        @asyncio.coroutine
+        async def __aenter__(self):
+            """We are our own iterator."""
+            return self.__enter__()
+        async def __aexit__(self, *args):#, exc_type, exc, traceback
+            await self.close()#return
         async def _async_httpRequest(self, url, method='GET', params=None, data=None, headers=None, cookies=None, json=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None, max_tries=3):
             from json.decoder import JSONDecodeError
             for x in range(max_tries):
