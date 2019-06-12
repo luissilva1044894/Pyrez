98eadf0ee56783075a38fe5642b4a2b6189e1ad1
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-12 19:43:16 -0300

    Update Winratio.py

--- pyrez/models/Mixin/Winratio.py
+++ pyrez/models/Mixin/Winratio.py
@@ -1,8 +1,11 @@
 class Winratio:
-    def __init__(self, **kwargs):
-        self.losses = kwargs.get('Losses', kwargs.get('losses', 0)) if kwargs else 0
-        self.wins = kwargs.get('Wins', kwargs.get('wins', 0)) if kwargs else 0
+	def __init__(self, **kwargs):
+        self.losses = kwargs.get('Losses', kwargs.get('losses', None)) or 0
+        self.wins = kwargs.get('Wins', kwargs.get('wins', None)) or 0
     @property
     def winratio(self):
-        _w = self.wins /((self.wins + self.losses) if self.wins + self.losses > 1 else 1) * 100.0
+        _w = self.wins /((self.matches_played) if self.matches_played > 1 else 1) * 100.0
         return int(_w) if _w % 2 == 0 else round(_w, 2)
+    @property
+    def matches_played(self):
+    	return self.wins + self.losses
