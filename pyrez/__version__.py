65afcf26860e6703fec609d7f56c68db6237bcc7
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-12 22:35:08 -0300

    Version bump to 1.1.1

--- pyrez/__version__.py
+++ pyrez/__version__.py
@@ -10,7 +10,7 @@ __description__ = "An open-source wrapper for Hi-Rez Studios API (Paladins, Real
 __license__ = "MIT"
 __package_name__ = "pyrez"
 __url__ = "https://github.com/luissilva1044894/{package_name}".format(package_name=__package_name__)#"https://{package_name}.readthedocs.io/en/stable".format(package_name=__package_name__)
-VERSION = (1, 1, 0, 0)
+VERSION = (1, 1, 1, 0)
 __version__ = '.'.join(str(v) for v in VERSION)#'.'.join(map(str, VERSION))
 __title__ = '{}/{}'.format(__package_name__.capitalize(), __version__)
 version = __version__
