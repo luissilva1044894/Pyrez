dc59824cd0ccaa40c7135c548b75adc3bf416ca8
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-14 15:41:07 -0300

    Update APIBase.py

--- pyrez/api/APIBase.py
+++ pyrez/api/APIBase.py
@@ -1,9 +1,3 @@
-from json.decoder import JSONDecodeError
-from sys import version_info
-
-import requests
-
-from pyrez import __version__ as pyrez
 class APIBase:
     #Do not instantiate this object directly; instead, use::
     """The constructor for APIBase class. DON'T INITALISE THIS YOURSELF!
@@ -26,11 +20,12 @@ class APIBase:
     """
     def __init__(self, headers=None, cookies=None, loggerName=None, debug_mode=True):
         from ..utils.http import get_user_agent
+        import requests
         self.debug_mode = debug_mode
         if self.debug_mode:
             from ..logging import create_logger
             self.logger = create_logger(loggerName or self.__class__.__name__)
-        self.headers = headers or headers or get_user_agent()
+        self.headers = headers or get_user_agent()
         self.cookies = cookies
         self.__session__ = requests.Session()
     def __enter__(self):
@@ -54,6 +49,7 @@ class APIBase:
         """
         return str(string).encode(encodeType)
     def _httpRequest(self, url, method="GET", raise_for_status=True, params=None, data=None, headers=None, cookies=None, json=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None):
+        from json.decoder import JSONDecodeError
         with self.__session__.request(method=method, url=url.replace(' ', '%20'), params=params, json=json, data=data, headers=headers or self.headers, cookies=cookies or self.cookies, files=files, auth=auth, timeout=timeout, allow_redirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert) as resp:
             self.cookies = resp.cookies
             if raise_for_status:
