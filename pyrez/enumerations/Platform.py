3faeb725ff9c13159b6731ab0ca94999bf02bc77
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-17 17:14:47 -0300

    Update Platform.py

--- pyrez/enumerations/Platform.py
+++ pyrez/enumerations/Platform.py
@@ -1,9 +1,10 @@
 from .Enum import Enum
 class Platform(Enum):
-    MOBILE = 'MOBILE'
-    NINTENDO_SWITCH = 'Nintendo'
-    PC = 'HiRez'
-    PS4 = 'PSN'
-    XBOX = 'XboxLive'
-    STEAM = 'Steam'
-    #UNKNOWN = 'unknown' #XBOX = 'xbox' #SWITCH = 'switch'
+	"""This is to refer to the platform always the same way and to prevent the changes if the api updates."""
+	MOBILE = 'MOBILE'
+	NINTENDO_SWITCH = 'Nintendo'
+	PC = 'HiRez'#pc
+	PS4 = 'PSN'
+	XBOX = 'XboxLive'#xbl
+	STEAM = 'Steam'
+	#UNKNOWN = 'unknown' #XBOX = 'xbox' #SWITCH = 'switch'
