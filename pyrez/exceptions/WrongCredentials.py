58ea5f526e65c0df4314beb6c1d0df4ab126feee
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-17 17:11:24 -0300

    Update WrongCredentials.py

--- pyrez/exceptions/WrongCredentials.py
+++ pyrez/exceptions/WrongCredentials.py
@@ -1,5 +1,5 @@
 from .PyrezException import PyrezException
 class WrongCredentials(PyrezException):
-	"""Raised when you try to access a resource and it fails due to some issue with your authentication."""
-	def __init__(self, *args, **kwargs):
+	"""Raised when an invalid or blocked Credentials is passed."""
+	def __init__(self, *args, **kwargs):#you try to access a resource and it fails due to some issue with your authentication
 		super().__init__(*args, **kwargs)
