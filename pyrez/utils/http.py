f939f49cc341db430d669471e4cdb626d9470ba9
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-17 15:21:52 -0300

    Update http.py

--- pyrez/utils/http.py
+++ pyrez/utils/http.py
@@ -2,4 +2,4 @@ def get_user_agent(dependencies, origin=None):
     from ..__version__ import __package_name__, __url__, __version__
     import sys
     __DEFAULT_USER_AGENT__ = '{pyrez} ({url} {ver}) [Python/{py.major}.{py.minor}.{py.micro} {dependencies.__name__}/{dependencies.__version__}]'.format(pyrez=__package_name__, url=__url__, ver=__version__, py=sys.version_info, dependencies=dependencies)
-    return {'User-Agent': __user_agent__, 'Origin': origin} if origin else {'User-Agent': __user_agent__}
+    return {'User-Agent': __DEFAULT_USER_AGENT__, 'Origin': origin} if origin else {'User-Agent': __DEFAULT_USER_AGENT__}
