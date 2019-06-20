7b717312b17b66f47613ca505cfcba6df75e0529
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-20 10:48:32 -0300

    Update PlayerNotFound.py

--- pyrez/exceptions/PlayerNotFound.py
+++ pyrez/exceptions/PlayerNotFound.py
@@ -1,5 +1,5 @@
 from .PyrezException import PyrezException
-class PlayerNotFound(PyrezException):#UnknownPlayerError
+class PlayerNotFound(PyrezException):#UnknownPlayer
 	"""Raises an error when a player does not exist via the API"""
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)#The specified player was not found
