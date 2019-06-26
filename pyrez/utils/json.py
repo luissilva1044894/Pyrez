34a577bc2d34d26c3aed30d9d981e2863dc37898
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-26 11:14:47 -0300

    Update json.py

--- pyrez/utils/json.py
+++ pyrez/utils/json.py
@@ -13,6 +13,7 @@ def get_json():
                 import json #raise ImportError('A json library is required to use this python library')
     return json
 def to_json(data, *, separators=(',', ':'), ensure_ascii=True, sort_keys=True, indent=2):
+    #use json.dumps instead?
     return get_json().dumps(data, separators=separators, ensure_ascii=ensure_ascii, sort_keys=sort_keys, indent=indent)
 def unpack_json(data):
     return get_json().loads(data)
@@ -44,11 +45,11 @@ def write(data, path, is_async=False, mode='w', encoding='utf-8'):
     if is_async:
         try:
             import aiofiles
-            async def __write__(path, mode='r', encoding='utf-8'):
+            async def __write__(data, path, mode='r', encoding='utf-8'):
                 #RuntimeWarning: coroutine 'write.<locals>.__write__' was never awaited
                 async with aiofiles.open(path, mode=mode, encoding=encoding) as f:
                     await f.write(to_json(data))
-            return __write__(path, mode, encoding)
+            return __write__(data, path, mode, encoding)
         except ImportError:
             pass
     """
