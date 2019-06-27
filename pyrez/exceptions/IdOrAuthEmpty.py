f8c2113074838bec34cbe5e3e701aa4afcb89f0b
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-27 09:58:27 -0300

    Delete IdOrAuthEmpty.py

--- pyrez/exceptions/IdOrAuthEmpty.py
+++
@@ -1,5 +0,0 @@
-from .PyrezException import PyrezException
-class IdOrAuthEmpty(PyrezException):#UnauthorizedError
-	"""Raises an error that the current Credentials is invalid or missing"""
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
