6fd235ed3f5049bb1d2d861f5b0b091df00998c1
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-27 10:20:19 -0300

    Delete WrongCredentials.py

--- pyrez/exceptions/WrongCredentials.py
+++
@@ -1,5 +0,0 @@
-from .PyrezException import PyrezException
-class WrongCredentials(PyrezException):
-	"""Raised when an invalid or blocked Credentials is passed."""
-	def __init__(self, *args, **kwargs):#you try to access a resource and it fails due to some issue with your authentication
-		super().__init__(*args, **kwargs)
