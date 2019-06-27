6159a41e969eb0e2ae3cd24f4f1c2694e62a7505
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-26 23:03:19 -0300

    Update PaladinsStrikeAPI.py

--- pyrez/api/PaladinsStrikeAPI.py
+++ pyrez/api/PaladinsStrikeAPI.py
@@ -1,5 +1,5 @@
 from .API import API
 from pyrez.enumerations import Endpoint, Format
 class PaladinsStrikeAPI(API):
-    def __init__(self, devId, authKey, responseFormat=Format.JSON, sessionId=None, storeSession=True):
-        super().__init__(devId, authKey, Endpoint.PALADINS_STRIKE, responseFormat, sessionId, storeSession)
+    def __init__(self, devId, authKey, response_format=Format.JSON, sessionId=None, storeSession=True):
+        super().__init__(devId, authKey, Endpoint.PALADINS_STRIKE, response_format, sessionId, storeSession)
