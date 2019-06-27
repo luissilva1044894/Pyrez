49ca8b147128bd1d6df0f1c78479277e4099f46c
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-27 09:58:31 -0300

    Delete DailyLimit.py

--- pyrez/exceptions/DailyLimit.py
+++
@@ -1,5 +0,0 @@
-from .PyrezException import PyrezException
-class DailyLimit(PyrezException):#RateLimitExceeded
-	"""Request rejected due to the rate limit being exceeded."""
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
