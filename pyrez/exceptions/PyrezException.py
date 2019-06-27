bbb09dbbe0443a6afd2af5a54eb8b9d52d93c75e
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-27 09:58:06 -0300

    Update PyrezException.py

--- pyrez/exceptions/PyrezException.py
+++ pyrez/exceptions/PyrezException.py
@@ -4,8 +4,3 @@ class PyrezException(Exception):
 		super().__init__(self, *args, **kwargs)
 	def __str__(self):
 		return str(self.args[1]) if self.args else 'An unknown error has occured within Pyrez'
-
-class InvalidTime(PyrezException):
-	"""Invalid timestamp"""
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
