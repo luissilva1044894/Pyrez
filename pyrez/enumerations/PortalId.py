67b93bb81fa66b0e16b91c2fad6ee9084a7a0ac0
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-07-30 10:01:23 -0300

    Add Mixer portalId

--- pyrez/enumerations/PortalId.py
+++ pyrez/enumerations/PortalId.py
@@ -1,9 +1,10 @@
 from .Enum import Enum
 class PortalId(Enum):
-    Unknown = -1
-    HiRez = 1
-    Steam = 5
-    PS4 = 9
-    Xbox = 10
-    Switch = 22
-    Discord = 25
+	Unknown = -1
+	HiRez = 1
+	Steam = 5
+	PS4 = 9
+	Xbox = 10
+	Mixer = 14
+	Switch = 22
+	Discord = 25
