fb70b53896e643d8b123a4a4b5475139e931c42c
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-01 20:45:19 -0300

    Update api_response.py

--- pyrez/models/api_response.py
+++ pyrez/models/api_response.py
@@ -4,30 +4,24 @@
 # -*- coding: utf-8 -*-
 
 from .mixins.dict import Dict
-class Base(Dict):
-	"""Superclass for all Pyrez models.
+class APIResponse(Dict):
+	"""Represents a generic Pyrez object. This is a super-class for all Pyrez models.
 	Keyword Arguments
 	-----------------
+	error_msg:
+	  The message returned from the API request.
 	json:
 		The request as JSON, if you prefer.
 	"""
 	def __init__(self, **kw):
 		super().__init__(**kw)
-
+		# self.content
+		# self.headers
+		# self.status
+		self.error_msg = kw.get('ret_msg', kw.get('error', kw.get('errors', None))) or None
 	@property
 	def json(self):
-		return self.__kwargs__ or {}
-
-class APIResponse(Base):
-	"""Represents a generic Pyrez object. This is a sub-class of :class:`APIResponseBase`.
-	Keyword Arguments
-	-----------------
-	error_msg:
-	  The message returned from the API request.
-	"""
-	def __init__(self, **kw):
-		super().__init__(**kw)
-		self.error_msg = kw.get('ret_msg', kw.get('error', kw.get('errors', None))) or None
+		return self.to_json()
 	@property
 	def has_error(self):
 		return self.error_msg is not None
