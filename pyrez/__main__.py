a44db4ff7e533789b66497432cc27c91b9173e58
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-04 20:28:47 -0300

    Update __main__.py

--- pyrez/__main__.py
+++ pyrez/__main__.py
@@ -1,7 +1,11 @@
 
 #!/usr/bin/env python
-# encoding: utf-8
 # -*- coding: utf-8 -*-
+# encoding: utf-8
+
+def main():
+	""" Main program """
+	return 0
 
 if __name__ == '__main__':
-	pass
+	main()
