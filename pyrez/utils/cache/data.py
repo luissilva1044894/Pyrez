d1938280f8a1fd28caace5ecb36be4934d6527cf
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-05 07:10:48 -0300

    Update data.py

--- pyrez/utils/cache/data.py
+++ pyrez/utils/cache/data.py
@@ -5,10 +5,10 @@
 
 class Data(dict):
   def __init__(self, *args, **kw):
-    from datetime import datetime, deltatime
+    from datetime import datetime
     # self.__create_at__, self.__timeout__ = datetime.utcnow(), kw.pop('timeout', 0)
     # self._cache = kw.pop('cache', None) or {}
-    self.__expires_at__ = datetime.utcnow() + kw.pop('timeout', 0)
+    self.__expires_at__ = datetime.utcnow() + kw.pop('timeout', 0) #Timeout(timeout)
     super().__init__(*args, **kw)
   def get(self, key, silent=True):
     if silent:
@@ -16,6 +16,10 @@ class Data(dict):
     return self._cache[key]
   def set(self, key, value):
     self._cache[key] = value
+  @property
+  def needs_refresh(self):
+    from datetime import datetime
+    return datetime.utcnow() >= self.__expires_at__
   def has_key(self, key):
     """Check whether the key exists."""
     return self.get(key.replace(' ', '_')) is not None
