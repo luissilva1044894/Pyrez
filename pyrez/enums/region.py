6ff83f78fe7e9e2d9d1a29a507b15db007fe017a
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-07 11:13:54 -0300

    Update region.py

--- pyrez/enums/region.py
+++ pyrez/enums/region.py
@@ -7,12 +7,18 @@ from . import Enum
 class Region(Enum):
 	UNKNOWN = ''
 	AUSTRALIA = 'Australia'
+	AUSTRALIA = 'oce'
+	AUSTRALIA = 'oceania'
+	AUSTRALIA = 'aus'
 	BRAZIL = 'Brazil'
 	EUROPE = 'Europe'
 	EUROPE = 'EU'
 	LATIN_AMERICA_NORTH = 'Latin America North'
+	LATIN_AMERICA_NORTH = 'latam'
 	NORTH_AMERICA = 'North America'
+	NORTH_AMERICA = 'NA'
 	SOUTHEAST_ASIA = 'Southeast Asia'
+	SOUTHEAST_ASIA = 'sea'
 
 __all__ = (
 	'Region',
