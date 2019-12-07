d75861ea39db3c78a6a0f96fabb56cbe9442d0a4
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-07 11:09:14 -0300

    Create portal.py

---
+++ pyrez/enums/portal.py
@@ -0,0 +1,30 @@
+
+#!/usr/bin/env python
+# encoding: utf-8
+# -*- coding: utf-8 -*-
+
+from . import Enum
+class PortalId(Enum):
+	Unknown = 0
+	Unknown = 'unknown'
+	HiRez = 1
+	HiRez = 'pc'
+	HiRez = 'Hirez'
+	Hirez = 'Hi-Rez'
+	Steam = 5
+	Steam = 'Steam'
+	PS4 = 9
+	PS4 = 'ps4'
+	PS4 = 'PSN'
+	Xbox = 10
+	Xbox = 'XboxLive'
+	Xbox = 'xbox'
+	Mixer = 14
+	Switch = 22
+	Switch = 'switch'
+	Discord = 25
+	Discord = 'Discord'
+
+__all__ = (
+	'PortalId',
+)
