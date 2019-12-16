834431b8b9cb27c80cc12a5d2084c605af951194
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-16 20:50:02 -0300

    Update language.py

--- pyrez/enums/language.py
+++ pyrez/enums/language.py
@@ -3,18 +3,18 @@
 # encoding: utf-8
 # -*- coding: utf-8 -*-
 
-from . import Enum
-class Language(Enum):
+from . import Named
+class Language(Named):
   English = 1
-  German = 2
-  French = 3
+  German = 2, 'Deutsch'
+  French = 3, 'Français'
   Chinese = 5
   Spanish = 7
-  Spanish_Latin_America = 9
-  Portuguese = 10
-  Russian = 11
-  Polish = 12
-  Turkish = 13
+  Spanish_Latin_America = 9, 'Español'
+  Portuguese = 10, 'Português do Brasil'
+  Russian = 11, 'Русский'
+  Polish = 12, 'Polski'
+  Turkish = 13, 'Türkçe'
 
 __all__ = (
   'Language',
