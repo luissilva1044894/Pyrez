4a43fe819f64168850fab943eb887ced1de7c0c9
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-19 06:49:55 -0300

    Update sync_usage.py

--- examples/sync_usage.py
+++ examples/sync_usage.py
@@ -9,18 +9,18 @@ fake_dev_id=1004
 fake_auth_key='23DF3C7E9BD14D84BF892AD206B6755C'
 
 def standard():
-	paladins = pyrez.api.Paladins(fake_dev_id, fake_auth_key)
-	print(paladins.data_used())
-	paladins.close()
+  paladins = pyrez.api.Paladins(fake_dev_id, fake_auth_key)
+  print(paladins.data_used())
+  paladins.close()
 
 def context_manager():
-	"""Context manager"""
-	with pyrez.api.Smite(fake_dev_id, fake_auth_key) as smite:
-		print(smite.data_used())
+  """Context manager"""
+  with pyrez.api.Smite(fake_dev_id, fake_auth_key) as smite:
+    print(smite.data_used())
 
 def main():
-	standard()
-	context_manager()
+  standard()
+  context_manager()
 
 if __name__ == '__main__':
-	main()
+  main()
