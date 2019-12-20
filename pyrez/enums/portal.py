9318fe90e7fd484ad7bb4303165d168009affddc
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-20 12:40:30 -0300

    Update portal.py

--- pyrez/enums/portal.py
+++ pyrez/enums/portal.py
@@ -4,27 +4,35 @@
 # -*- coding: utf-8 -*-
 
 from . import Enum
-class PortalId(Enum):
-  Unknown = 0
-  Unknown = 'unknown'
-  HiRez = 1
-  HiRez = 'pc'
-  HiRez = 'Hirez'
-  Hirez = 'Hi-Rez'
-  Steam = 5
-  Steam = 'Steam'
+class Portal(Enum):
+  UNKNOWN = 0
+  HIREZ = 1
+  HIREZ = '1'
+  HIREZ = 'pc'
+  HIREZ = 'hirez'
+  HIREZ = 'hi-rez'
+  STEAM = 5
+  STEAM = '5'
+  STEAM = 'steam'
   PS4 = 9
+  PS4 = '9'
   PS4 = 'ps4'
-  PS4 = 'PSN'
-  Xbox = 10
-  Xbox = 'XboxLive'
-  Xbox = 'xbox'
-  Mixer = 14
-  Switch = 22
-  Switch = 'switch'
-  Switch = 'nintendo_switch'
-  Discord = 25
-  Discord = 'Discord'
+  PS4 = 'psn'
+  PTS = 'pts'
+  XBOX = 10
+  XBOX = '10'
+  XBOX = 'xboxlive'
+  XBOX = 'xbox'
+  MIXER = 14
+  MIXER = '14'
+  MIXER = 'mixer'
+  NINTENDO_SWITCH = 22
+  NINTENDO_SWITCH = '22'
+  NINTENDO_SWITCH = 'switch'
+  NINTENDO_SWITCH = 'nintendo_switch'
+  DISCORD = 25
+  DISCORD = '25'
+  DISCORD = 'discord'
 
 __all__ = (
   'PortalId',
