d192606383ab40743ac3b9e8f202a31e095590a1
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-21 12:44:34 -0300

    Delete oauth.py

--- pyrez/enums/portal.py
+++ pyrez/enums/portal.py
@@ -8,28 +8,28 @@ class Portal(Enum):
   UNKNOWN = 0
   HIREZ = 1
   HIREZ = 'hi-rez'
-  HIREZ = 'hirez'
   HIREZ = 'pc'
   STEAM = 5
-  STEAM = 'steam'
-  PS4 = 9
-  PS4 = 'playstation'
-  PS4 = 'ps4'
-  PS4 = 'psn'
+  PLAY_STATION = 9
+  PLAY_STATION = 'playstation'
+  PLAY_STATION = 'ps4'
+  PLAY_STATION = 'psn'
   PTS = 'pts'
+  PTS = 'public_test_server'
   XBOX = 10
   XBOX = 'xb'
-  XBOX = 'xbox'
   XBOX = 'xbox_one'
   XBOX = 'xbox1'
   XBOX = 'xboxlive'
   MIXER = 14
-  MIXER = 'mixer'
   NINTENDO_SWITCH = 22
-  NINTENDO_SWITCH = 'nintendo_switch'
   NINTENDO_SWITCH = 'switch'
   DISCORD = 25
-  DISCORD = 'discord'
+  PALADINS_STRIKE = 'paladinsstrike'
+  FACEBOOK = 'facebook'
+  FACEBOOK = 'facebookpaladins'
+  GOOGLE = 'google'
+  TWITCH = 'twitch'
 
   def icon(self, c=None):
     if self not in [Portal.UNKNOWN, Portal.PTS]:
@@ -38,6 +38,12 @@ class Portal(Enum):
         from ..utils.http import img_download
         return img_download(__url__, c)
       return __url__
+
+  def oauth_url(self, api=None, param=None):
+    value = {Portal.FACEBOOK:'facebook', Portal.GOOGLE:'google', Portal.HIREZ:'hirez', Portal.STEAM:'steam', Portal.PLAY_STATION:'playstation', Portal.XBOX:'xbox', Portal.NINTENDO_SWITCH:'switch', Portal.DISCORD:'discord'}.get(self)
+    if value:
+      from .endpoint import Endpoint
+      return f'{api or Endpoint.HIREZ}/oauth/{"" if self == Portal.HIREZ else "out/"}{value}?{"redirect_uri" if self == Portal.HIREZ else "url"}={param or ""}'
 
 __all__ = (
   'Portal',
