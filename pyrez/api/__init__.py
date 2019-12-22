48c35fe466f694270e258231d59d90d4b3597c53
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-22 17:21:35 -0300

    Update __init__.py

--- pyrez/api/__init__.py
+++ pyrez/api/__init__.py
@@ -37,10 +37,10 @@ class API(Base):
         from ..models.session import Session
         path = f'{get_path(root=True)}\\data\\{self.dev_id}.json'
         '''
-        if self._is_async:
-          f = self.loop.create_task(read_file(path, is_async=self._is_async, is_json=True))
+        if self.is_async:
+          f = self.loop.create_task(read_file(path, is_async=self.is_async, is_json=True))
         else:
-          f = read_file(path, is_async=self._is_async, is_json=True)
+          f = read_file(path, is_async=self.is_async, is_json=True)
         session = Session(api=self, **f)
         '''
         session = Session(api=self, **read_file(path, is_json=True))
@@ -58,11 +58,11 @@ class API(Base):
       from ..utils.file import write_file, get_path
       path = f'{get_path(root=True)}\\data\\{self.dev_id}.json'
       '''
-      if self._is_async:
-        self.loop.create_task(write_file(path, session.json, is_async=self._is_async, is_json=True))
+      if self.is_async:
+        self.loop.create_task(write_file(path, session.json, is_async=self.is_async, is_json=True))
       else:
-        write_file(path, session.json, is_async=self._is_async, is_json=True)
-      #write_file(path, session, is_async=self._is_async)
+        write_file(path, session.json, is_async=self.is_async, is_json=True)
+      #write_file(path, session, is_async=self.is_async)
       '''
       write_file(path, session.json, is_json=True)
   @property
@@ -126,53 +126,57 @@ class API(Base):
       from ..exceptions.UnauthorizedError import UnauthorizedError
       raise UnauthorizedError(error_msg)
   '''
+
   def request(self, api_method=None, params=(), **kw):
     from ..exceptions.invalid_session_id import InvalidSessionId
     from ..exceptions.invalid_argument import InvalidArgument
     from ..utils import ___
     from ..utils.cache.data import Data
-    _cls, raises, __cls__ = kw.pop('cls', None), kw.pop('raises', None), self.__class__.__name__.lower()
-    _wants_update_ = kw.pop('force', not kw.pop('cached', True) and api_method not in cache._defaults[__cls__].keys() or cache._defaults[__cls__].get(api_method, {}).get('optional')) or (not cache.has_key(__cls__) or cache.has_key(__cls__) and not cache.get(__cls__).get(api_method) or cache.has_key(__cls__) and cache.get(__cls__).get(api_method).needs_refresh)
+    #_cls, raises, __cls__ = kw.pop('cls', None), kw.pop('raises', None), self.__class__.__name__.lower()
+    _cls, raises, __cls__, _params_, __filter__, __api__ = kw.pop('cls', None), kw.pop('raises', None), (self.__endpoint__.name if hasattr(self, '__endpoint__') else self.__class__.__name__).upper(), None if not params else '_'.join([str(_).upper() for _ in params if _]) if isinstance(params, (list, tuple)) else str(params), kw.pop('filter', None), kw.pop('api', None) or self
+    _wants_update_ = cache.wants_update(api_method, __cls__, _params_, force=kw.pop('force', not kw.pop('cached', True)))
+    #_wants_update_ = kw.pop('force', not kw.pop('cached', True) and api_method not in cache._defaults.get(__cls__).keys() or cache._defaults.get(__cls__, {}).get(api_method, {}).get('optional')) or (not cache.has_key(__cls__) or cache.has_key(__cls__) and (not cache.get(__cls__).get(api_method) or cache.get(__cls__).get(api_method).needs_refresh or _params_ and not cache.get(__cls__).get(f'{api_method},{_params_}') or cache.get(__cls__).get(f'{api_method},{_params_}').needs_refresh))
     #_json = kw.pop('json', str(self._response_format) == 'json')
     if api_method:
-      if self._is_async:
-        async def __make_request__():#api_method=None, params=(), **kw):
+      if self.is_async:
+        async def __request__(api_method=None, params=(), **kw):
           try:
             if _wants_update_:
               if not api_method.lower() in [__methods__[0], __methods__[-2], __methods__[-1]] and self._invalid_session_id:
                 raise InvalidSessionId
               #return asyncio.ensure_future(self.http.get(self._build_url_(api_method, params), json=_json))
               r = self._check_response_(await self.http.get(api_method if str(api_method).lower().startswith('http') else self._build_url_(api_method, params), **kw))
-              cache.set(self.__class__.__name__, r, api_method)
+              cache.set(__cls__, r, sub_key=api_method if not _params_ else f'{api_method},{_params_}')
               cache.save()
             else:
-              r = cache.get(self.__class__.__name__.lower()).get(api_method)
+              r = cache.get(__cls__, sub_key=api_method if not _params_ else f'{api_method},{_params_}')
               if hasattr(r, 'value') and isinstance(r, Data):
                 r = r.value
-              if not _cls:
-                return r or None
-              return ___(r, _cls, raises, api=self)
+            if not _cls:
+              return r or None
+            return ___(r, _cls, raises, filter=__filter__, api=__api__)
           except InvalidSessionId:
             await self._create_session()
-            return await self.request(api_method=api_method, params=params, cls=_cls, raises=raises, **kw)#json=_json,
-        return __make_request__()
+            return await self.request(api_method=api_method, params=params, cls=_cls, force=_wants_update_, raises=raises, api=__api__, **kw)
+        return __request__(api_method=api_method, params=params, **kw)
       try:
         if _wants_update_:
           if not api_method.lower() in [__methods__[0], __methods__[-2], __methods__[-1]] and self._invalid_session_id:
             raise InvalidSessionId
-          r = self._check_response_(self.http.get(api_method if str(api_method).lower().startswith('http') else self._build_url_(api_method, params), **kw))
+          import re
+          r = self._check_response_(self.http.get(api_method if re.match('(?i)^(https|http)?://.*$', str(api_method).lower()) else self._build_url_(api_method, params), **kw))
           '''
           try:
             print('try try')
-            __timeout__ = kw.pop('timeout', cache._defaults.get(self.__class__.__name__.lower()).get(api_method).get('timeout')) or cache._defaults.get(self.__class__.__name__.lower()).get('timeout')
+            __timeout__ = kw.pop('timeout', cache._defaults.get(__cls__).get(api_method).get('timeout')) or cache._defaults.get(__cls__).get('timeout')
           except AttributeError:
             print('try except')
-            __timeout__ = cache._defaults.get(self.__class__.__name__.lower()).get('timeout')
+            __timeout__ = cache._defaults.get(__cls__).get('timeout')
           '''
-          cache.set(self.__class__.__name__, r, api_method)
+          cache.set(__cls__, r, sub_key=api_method if not _params_ else f'{api_method},{_params_}')
           cache.save()
         else:
-          r = cache.get(self.__class__.__name__.lower()).get(api_method)
+          r = cache.get(__cls__, sub_key=api_method if not _params_ else f'{api_method},{_params_}')
           if hasattr(r, 'value') and isinstance(r, Data):
             r = r.value
           '''
@@ -182,12 +186,12 @@ class API(Base):
             except (ValueError, TypeError):
               pass
           '''
-          if not _cls:
-            return r or None
-          return ___(r, _cls, raises, api=self)
+        if not _cls:
+          return r or None
+        return ___(r, _cls, raises, filter=__filter__, api=__api__)
       except InvalidSessionId:
         self._create_session()
-        return self.request(api_method=api_method, params=params, cls=_cls, raises=raises, **kw)# json=_json,
+        return self.request(api_method=api_method, params=params, cls=_cls, force=_wants_update_, raises=raises, api=__api__, **kw)# json=_json,
     raise InvalidArgument('No API method specified!')
     '''
     try:
@@ -198,15 +202,6 @@ class API(Base):
     else:
       return _
     '''
-
-  def __request__(self, method, cls, params=(), raises=None):
-    if self._is_async:
-      async def __async_request__(method, cls, params=(), raises=None):
-        from ..utils import ___
-        return ___(await self.request(method, params), cls, raises, api=self)
-      return __async_request__(method, cls, params, raises)
-    from ..utils import ___
-    return ___(self.request(method, params), cls, raises, api=self)
 
   def create_signature(self, api_method, timestamp=None):
     from ..utils.time import get_timestamp
@@ -237,23 +232,24 @@ class API(Base):
           if isinstance(params, (list, tuple)):
             from datetime import datetime
             from enum import Enum
-            url += '/'.join(p.strftime('yyyyMMdd') if isinstance(p, datetime) else str(p.value) if isinstance(p, Enum) else str(p) for p in params if p)
+            url += '/' + '/'.join(p.strftime('yyyyMMdd') if isinstance(p, datetime) else str(p.value) if isinstance(p, Enum) else str(p) for p in params if p)
           else:
             url += f'/{params}'
+      print(url)
       return url
     raise InvalidArgument('No API method specified!')
 
-  def info(self):
+  def info(self, **kw):
     from ..utils.file import read_file, get_path
     from ..utils import slugify
     path = f'{get_path(__file__, root=True)}\\data\\links.json'
     return read_file(path)[slugify(self.__endpoint__.name).replace('_', '-')]#['website']['api']
 
   # GET /createsession[response_format]/{dev_id}/{signature}/{timestamp}
-  def _create_session(self):
+  def _create_session(self, **kw):
     from ..models.session import Session
     #return self.__request__(__methods__[0], Session)
-    return self.__request__(__methods__[0], cls=Session)
+    return self.request(__methods__[0], cls=kw.pop('cls', Session), **kw)
     #_ = self.request(__methods__[0])
     #if not _:
       #return None
@@ -265,72 +261,73 @@ class API(Base):
     """/ping[response_format] GET
     A quick way of validating access (establish connectivity) to the API."""
     from ..models.ping import Ping
-    return self.request(__methods__[-2], cls=Ping, **kw)
+    return self.request(__methods__[-2], cls=kw.pop('cls', Ping), **kw)
 
   # GET /testsession[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}
-  def test_session(self, session_id=None):
-    return self.request(__methods__[-1], params=session_id or self.session_id)
+  def test_session(self, session_id=None, **kw):
+    return self.request(__methods__[-1], params=session_id or self.session_id, **kw)
 
   # GET /getdataused[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}
-  def data_used(self):
-    return self.request(__methods__[1])
+  def data_used(self, **kw):
+    return self.request(__methods__[1], **kw)
 
   # GET /gethirezserverstatus[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}
   @cache.defaults(__methods__[1], timeout=15)
-  def server_status(self):
-    return self.request(__methods__[2])
+  def server_status(self, **kw):
+    return self.request(__methods__[2], **kw)
 
   # GET /getpatchinfo[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}
-  def patch_info(self):
-    return self.request(__methods__[-3])
+  def patch_info(self, **kw):
+    return self.request(__methods__[-3], **kw)
 
   # GET /getfriends[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{player_id}
-  def friends(self, player_id):
-    from ..models.player import Player
-    return self.request('getfriends', cls=Player, params=player_id)#[_ for _ in r if _.get('player_id', 0) not in [0, '0']]
+  def friends(self, player_id, **kw):
+    from ..models.player import _Base
+    return self.request('getfriends', params=player_id, cls=kw.pop('cls', _Base), filter='player_id', **kw)#[_ for _ in r if _.get('player_id', 0) not in [0, '0']]
 
   # GET /getmatchdetails[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{match_id}
   # GET /getmatchdetailsbatch[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{match_id,match_id,match_id,..,match_id}
   # GET /getmatchplayerdetails[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{match_id}
-  def match(self, match_id, is_live=False):
+  def match(self, match_id, is_live=False, **kw):
     if isinstance(match_id, (list, tuple)):
       mthd_name, params = 'getmatchdetailsbatch', ','.join((str(_) for _ in match_id))
     else:
       mthd_name, params = 'getmatchplayerdetails' if is_live else 'getmatchdetails', match_id
-    return self.request(mthd_name, params=params)
+    return self.request(mthd_name, params=params, **kw)
 
   # GET /getmatchidsbyqueue[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{queue_id}/{date}/{hour}
-  def match_ids(self, queue_id, data=None, hour=-1):
-    return self.request('getmatchidsbyqueue', params=[queue_id, data, hour])
+  def match_ids(self, queue_id, data=None, hour=-1, **kw):
+    return self.request('getmatchidsbyqueue', params=[queue_id, data, hour], **kw)
 
   # GET /getplayerachievements[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{player_id}
-  def player_achievements(self, player_id):
-    return self.request('getplayerachievements', params=player_id)
+  def player_achievements(self, player_id, **kw):
+    return self.request('getplayerachievements', params=player_id, **kw)
 
   # GET /getplayeridbyname[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{player_name}
   # GET /getplayeridbyportaluserid[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{portal_id}/{portal_user_id}
   # GET /getplayeridsbygamertag[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{portal_id}/{gamer_tag}
   # GET /getplayeridinfoforxboxandswitch[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{player_name}
-  def player_id(self, player_name, portal_id=None, *, xbox_or_switch=None):
-    from ..models.player import Player
+  def player_id(self, player_name, portal_id=None, *, xbox_or_switch=None, **kw):
+    from ..models.player import _Base
     if xbox_or_switch:
-      return self.request('getplayeridinfoforxboxandswitch', cls=Player, params=player_name)
+      return self.request('getplayeridinfoforxboxandswitch', params=player_name, cls=kw.pop('cls', _Base), **kw)
     if not portal_id:
-      #return self.__request__('getplayeridbyname', Player, params=player_name)
-      return self.request('getplayeridbyname', cls=Player, params=player_name)
-    return self.request('getplayeridbyportaluserid' if str(player_name).isnumeric() else 'getplayeridsbygamertag', cls=Player, params=[portal_id, player_name])
+      #return self.__request__('getplayeridbyname', _Base, params=player_name)
+      return self.request('getplayeridbyname', params=player_name, cls=kw.pop('cls', _Base), **kw)
+    return self.request('getplayeridbyportaluserid' if str(player_name).isnumeric() else 'getplayeridsbygamertag', params=[portal_id, player_name], cls=kw.pop('cls', _Base), **kw)
 
   # GET /getplayerstatus[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{player_id}
-  def player_status(self, player_id):
-    return self.request('getplayerstatus', params=player_id)
+  def player_status(self, player_id, **kw):
+    from ..models.player.status import Status
+    return self.request('getplayerstatus', params=player_id, cls=kw.pop('cls', Status), **kw)
 
   # GET /getqueuestats[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{player_id}/{queue_id}
-  def queue_stats(self, player_id, queue_id):
-    return self.request('getqueuestats', params=[player_id, queue_id])
+  def queue_stats(self, player_id, queue_id, **kw):
+    return self.request('getqueuestats', params=[player_id, queue_id], **kw)
 
   # GET /searchplayers[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{search_player}
-  def search_players(self, player_name):
-    return self.request('searchplayers', params=player_name)
+  def search_players(self, player_name, **kw):
+    return self.request('searchplayers', params=player_name, **kw)
 
 __all__ = (
   'API',
