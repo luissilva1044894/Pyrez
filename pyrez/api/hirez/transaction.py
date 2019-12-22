3f0650615c8f57a61e2487fffa2923ffd8165b0f
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-22 17:23:19 -0300

    Update transaction.py

--- pyrez/api/hirez/transaction.py
+++ pyrez/api/hirez/transaction.py
@@ -1,20 +1,20 @@
 
 #!/usr/bin/env python
-# encoding: utf-8
 # -*- coding: utf-8 -*-
+# encoding: utf-8
 
 from ...models.api_response import APIResponse
 class Transaction(APIResponse):
-  def __init__(self, **kwargs):
-    super().__init__(**kwargs)
+  def __init__(self, **kw):
+    super().__init__(**kw)
     from ...utils.time import iso_or_string
     from ...utils.num import num_or_string
-    self.creation_date = iso_or_string(kwargs.get('creation_dt')) or None
-    self.order_num = num_or_string(kwargs.get('order_no')) or 0
-    self.ip_address = kwargs.get('ip_address') or None
-    self.item_name = kwargs.get('item_name') or None
-    self.currency = kwargs.get('currency_cd') or None
-    self.price = num_or_string(kwargs.get('price')) or 0
-    self.gift_player = kwargs.get('gift_recipient_player') or None
-    self.gift_sender = kwargs.get('gift_sender_name') or None
-    self.gift_message = kwargs.get('gift_message') or None
+    self.creation_date = iso_or_string(kw.get('creation_dt')) or None
+    self.order_num = num_or_string(kw.get('order_no')) or 0
+    self.ip_address = kw.get('ip_address') or None
+    self.item_name = kw.get('item_name') or None
+    self.currency = kw.get('currency_cd') or None
+    self.price = num_or_string(kw.get('price')) or 0
+    self.gift_player = kw.get('gift_recipient_player') or None
+    self.gift_sender = kw.get('gift_sender_name') or None
+    self.gift_message = kw.get('gift_message') or None
