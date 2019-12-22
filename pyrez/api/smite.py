3af156c2682c0d61401752163cc2e92e1edeb053
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-22 17:23:40 -0300

    Update smite.py

--- pyrez/api/smite.py
+++ pyrez/api/smite.py
@@ -6,29 +6,29 @@
 from .base_paladins_smite import BasePaladinsSmite
 class Smite(BasePaladinsSmite):
   # GET /getgodrecommendeditems[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{god_id}/{language_code}
-  def god_recommended_items(self, god_id, language=None):
+  def god_recommended_items(self, god_id, language=None, **kw):
     from ..enums.language import Language
-    return self.request('getgodrecommendeditems', params=[god_id, language or Language.English])
+    return self.request('getgodrecommendeditems', params=[god_id, Language(language)], **kw)
 
   # GET /getmotd[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}
-  def motd(self):
-    return self.request('getmotd')
+  def motd(self, **kw):
+    return self.request('getmotd', **kw)
 
   # GET /getteamdetails[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{clan_id}
-  def team_details(self, clan_id):
-    return self.request('getteamdetails', params=clan_id)
+  def team_details(self, clan_id, **kw):
+    return self.request('getteamdetails', params=clan_id, **kw)
 
   # GET /getteamplayers[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{clan_id}
-  def team_players(self, clan_id):
-    return self.request('getteamplayers', params=clan_id)
+  def team_players(self, clan_id, **kw):
+    return self.request('getteamplayers', params=clan_id, **kw)
 
   # GET /gettopmatches[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}
-  def top_matches(self):
-    return self.request('gettopmatches')
+  def top_matches(self, **kw):
+    return self.request('gettopmatches', **kw)
 
   # GET /searchteams[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{search_team}
-  def search_teams(self, search_team):
-    return self.request('searchteams', params=search_team)
+  def search_teams(self, search_team, **kw):
+    return self.request('searchteams', params=search_team, **kw)
 
 __all__ = (
   'Smite',
