de5e2a18d3264256edea151d0bef647f88a7c933
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-23 09:36:55 -0300

    Update status.py

--- pyrez/models/player/status.py
+++ pyrez/models/player/status.py
@@ -6,23 +6,39 @@
 from ..api_response import APIResponse
 #from ...utils import decorators
 class Status(APIResponse):
-  def __init__(self, *, api=None, **kw):
-    super().__init__(**kw)
-    from ...enums import status
-    from ...utils.num import num_or_string
-    self.match_id = num_or_string(kw.get('Match') or kw.get('match_id')) or 0
-    self.queue_id = num_or_string(kw.get('match_queue_id')) or 0
-    self.status = status.Status(kw.get('status_id') or kw.get('status') or 5)
-    self.status_message = kw.get('personal_status_message') or None
-    self.status_string = kw.get('status_string') or kw.get('status') or None
-    self.__api__ = api
+
   @property
   def in_match(self):
-    return self.match_id > 0
-  #@decorators.has_match_id
-  def get_match_details(self):
+    return self.match_id != 0 or self.status == 3
+
+  def match_details(self, **kw):
     if self.in_match:
-      return self.__api__.match(self.match_id, is_live=True)
-  def get_players_from_match(self):
+      return self.__api__.match(self.match_id, is_live=True, **kw)
+
+  @property
+  def match_id(self):
+    from ...utils.num import num_or_string
+    return num_or_string(kw.get('Match') or kw.get('match_id')) or 0
+
+  def match_players(self, **kw):
     if self.in_match:
-      return self.__api__.players_from_match(self.match_id)
+      return self.__api__.players_from_match(self.match_id, **kw)
+
+  @property
+  def queue_id(self):
+    from ...utils.num import num_or_string
+    return num_or_string(self.get('match_queue_id')) or 0
+
+  @property
+  def status(self):
+    from ...enums import status
+    from ...utils.num import num_or_string
+    return status.Status(num_or_string(self.get('status_id') or self.get('status')) or 0) or 5
+
+  @property
+  def status_message(self):
+    return self.get('personal_status_message') or None
+
+  @property
+  def status_string(self):
+    return self.get('status_string') or kw.get('status') or None
