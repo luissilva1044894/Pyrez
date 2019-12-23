ae007b72b2d725fe8d8885c829e0443cca8f82d5
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-23 09:38:29 -0300

    Update dict.py

--- pyrez/models/mixins/dict.py
+++ pyrez/models/mixins/dict.py
@@ -7,26 +7,24 @@ class Dict(dict):
   def __init__(self, **kw):
     """It can act both as a dictionary (c['foo']) and as an object (c.foo) to get values."""
     dict.__init__(self, kw or {})
-    self.__kwargs__ = kw or []
+    self.__kw__ = kw or []
   '''
   def get(self, key, default=None):
-    return self.__kwargs__.get(key, default)
+    return self.__kw__.get(key, default)
   def __getitem__(self, key):
     try:
-        return self.__kwargs__[key]
+        return self.__kw__[key]
     except KeyError:
         return None
   '''
+  def __str__(self):
+    return self.json
+
   def to_json(self):
     import json
-    return json.dumps(self.__kwargs__ or {}, ensure_ascii=False, sort_keys=False, separators=(',', ':'), indent=2)
+    return json.dumps(self.__kw__ or {}, ensure_ascii=False, sort_keys=False, separators=(',', ':'), indent=2)
     #from ...utils.json import dumps
-    #dumps(self.__kwargs__ or {}, ensure_ascii=False, sort_keys=False, separators=(',', ':'), indent=2)
+    #dumps(self.__kw__ or {}, ensure_ascii=False, sort_keys=False, separators=(',', ':'), indent=2)
   @property
   def json(self):
     return self.to_json()
-
-  '''
-  def __str__(self):
-    return self.to_json()
-  '''
