29fe2727fc7abdf9d0e7831e9e731f3ed71b20a6
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-23 09:56:02 -0300

    Update __init__.py

--- pyrez/api/paladins/player/__init__.py
+++ pyrez/api/paladins/player/__init__.py
@@ -8,12 +8,15 @@ from ....utils import decorators
 class Player(player.Player):
   def __init__(self, *, api=None, **kw):
     super().__init__(api=api, **kw)
+
   @decorators.is_public
   def loadouts(self, **kw):
     return self.__api__.player_loadouts(self.id, api=self.__api__, **kw)
+
   @decorators.is_public
   def champions(self, **kw):
     return self.__api__.player_champions(self.id, api=self.__api__, **kw)
+
   @decorators.is_public
   def champions_ranks(self, god_id, **kw):
     from ....enums.champions import Champion
