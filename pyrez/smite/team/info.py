ef2df60b7cebae3f3f021a7f143d64d77027ed6e
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-25 17:46:52 -0300

    Update info.py

--- pyrez/smite/team/info.py
+++ pyrez/smite/team/info.py
@@ -5,6 +5,23 @@
 
 from . import Team
 class Info(Team):
-  def __init__(self, *, api=None, **kw):
-    super().__init__(api=api, **kw)
-    self.rating = kw.get('Rating') or 0
+
+  @property
+  def rating(self):
+    from ...utils.num import num_or_string
+    return num_or_string(self.json.get('Rating')) or 0
+  
+  @property
+  def losses(self):
+    from ...utils.num import num_or_string
+    return num_or_string(self.json.get('Losses')) or 0
+  
+  @property
+  def wins(self):
+    from ...utils.num import num_or_string
+    return num_or_string(self.json.get('Wins')) or 0
+
+  @property
+  def winratio(self):
+    from ...utils.num import winratio
+    return winratio(self.wins, self.wins + self.losses)
