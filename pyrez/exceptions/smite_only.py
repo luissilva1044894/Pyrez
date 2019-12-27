504f372f6139493734b31eee301a425eefaa8017
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-27 20:32:35 -0300

    Update smite_only.py

--- pyrez/exceptions/smite_only.py
+++ pyrez/exceptions/smite_only.py
@@ -3,8 +3,8 @@
 # encoding: utf-8
 # -*- coding: utf-8 -*-
 
-from .__init__ import PyrezException 
-class SmiteOnly(PyrezException):
+from .not_supported import NotSupported
+class SmiteOnly(NotSupported):
   def __init__(self, *args, **kw):
     super().__init__(*args, **kw)
 
