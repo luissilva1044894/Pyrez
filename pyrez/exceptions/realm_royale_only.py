4edd84fde8b60e91e990542a1abdc1af7739e134
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-27 20:32:59 -0300

    Update realm_royale_only.py

--- pyrez/exceptions/realm_royale_only.py
+++ pyrez/exceptions/realm_royale_only.py
@@ -3,8 +3,8 @@
 # encoding: utf-8
 # -*- coding: utf-8 -*-
 
-from .__init__ import PyrezException 
-class RealmRoyaleOnly(PyrezException):
+from .not_supported import NotSupported
+class RealmRoyaleOnly(NotSupported):
   def __init__(self, *args, **kw):
     super().__init__(*args, **kw)
 
