e2827682e249d56219be3a2ba9dd7b06ec8a3a46
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-27 20:33:47 -0300

    Update invalid_time.py

--- pyrez/exceptions/invalid_time.py
+++ pyrez/exceptions/invalid_time.py
@@ -3,8 +3,8 @@
 # encoding: utf-8
 # -*- coding: utf-8 -*-
 
-from .__init__ import PyrezException 
-class InvalidTime(PyrezException):
+from .invalid_argument import InvalidArgument
+class InvalidTime(InvalidArgument):
   """Invalid timestamp"""
   def __init__(self, *args, **kw):
     super().__init__(*args, **kw)
