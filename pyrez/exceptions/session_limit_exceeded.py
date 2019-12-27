86752bd67e4b661f5f750b4c82471e37e4529a51
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-27 20:32:43 -0300

    Update session_limit_exceeded.py

--- pyrez/exceptions/session_limit_exceeded.py
+++ pyrez/exceptions/session_limit_exceeded.py
@@ -3,8 +3,8 @@
 # encoding: utf-8
 # -*- coding: utf-8 -*-
 
-from .__init__ import PyrezException 
-class SessionLimitExceeded(PyrezException):
+from .rate_limit_exceeded import RateLimitExceeded
+class SessionLimitExceeded(RateLimitExceeded):
   """Raised when the maximum number of active sessions is reached."""
   def __init__(self, *args, **kw):
     super().__init__(*args, **kw)
