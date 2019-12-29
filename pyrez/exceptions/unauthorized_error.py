a786cdaf025448b70924e1426f4aa1a116701893
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-29 19:14:52 -0300

    Update unauthorized_error.py

--- pyrez/exceptions/unauthorized_error.py
+++ pyrez/exceptions/unauthorized_error.py
@@ -5,7 +5,7 @@
 
 from .__init__ import PyrezException
 class UnauthorizedError(PyrezException):
-  """Raised when the current Credentials is invalid, blocked or missing"""
+  """This exception is raised when the passed Credentials is invalid, blocked or missing."""
   def __init__(self, *args, **kw):
     #you try to access a resource and it fails due to some issue with your authentication
     super().__init__(*args, **kw)
