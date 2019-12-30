b2debe704c940d945177ce60bd83a3e3e4eef6a7
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-30 01:33:30 -0300

    Update ping.py

--- pyrez/models/ping.py
+++ pyrez/models/ping.py
@@ -1,17 +1,18 @@
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # encoding: utf-8
 
-class Ping:
+from .api_response import APIResponse
+class Ping(APIResponse):
   def __init__(self, v, *args, **kw):
-    self.json = str(v)
-    _js = str(v).split(' ')
+    _js, _json = str(v).split(' '), {}
     if len(_js) > 11:
       from datetime import datetime
-      self.api_name = _js[0]
-      self.api_version = _js[2].replace(')', '')
-      self.game_patch = _js[5].replace(']', '')
-      self.ping = _js[8] == 'successful.'
-      self.date = datetime.strptime('{} {} {}'.format(_js[10].replace('Date:', ''), _js[11], _js[12]), '%m/%d/%Y %I:%M:%S %p')
-  def __str__(self):
-    return f'API Name: {self.api_name} API Version: {self.api_version} Game Version: {self.game_patch} Date: {self.date}'
+      _json['api_name'] = _js[0]
+      _json['api_version'] = _js[2].replace(')', '')
+      _json['game_patch'] = _js[5].replace(']', '')
+      _json['ping'] = _js[8] == 'successful.'
+      #_json['date'] = datetime.strptime('{} {} {}'.format(_js[10].replace('Date:', ''), _js[11], _js[12]), '%m/%d/%Y %I:%M:%S %p')
+      _json['timestamp'] = f'{_js[10].replace("Date:", "")} {_js[11]} {_js[12]}'
+    super().__init__(*args, **{**_json, **kw})
