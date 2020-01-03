cea91b912e0c242156461b0e5b3958a3ecc5e4be
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2020-01-02 23:20:12 -0300

    Update endpoint.py

--- pyrez/enums/endpoint.py
+++ pyrez/enums/endpoint.py
@@ -7,13 +7,21 @@ from . import Enum
 class Endpoint(Enum):
   """Representing an endpoint that you want to access to retrieve information from."""
   PALADINS = 'http://api.paladins.com/paladinsapi.svc'
-  PALADINS = 400
   REALM_ROYALE = 'http://api.realmroyale.com/realmapi.svc'
   REALM_ROYALE = 'realm'
   REALM_ROYALE = 'realmroyale'
+  REALM_ROYALE = 'realm_royale_pc'
+  REALM_ROYALE = 'realm_royale_ps4'
+  REALM_ROYALE = 'realm_royale_switch'
+  REALM_ROYALE = 'realm_royale_xbox'
   SMITE = 'http://api.smitegame.com/smiteapi.svc'
-  SMITE = 300
+  SMITE = '300'
+  SMITE = 'smite_pc'
+  SMITE = 'smite_ps4'
+  SMITE = 'smite_switch'
+  SMITE = 'smite_xbox'
   HIREZ = 'https://api.hirezstudios.com'
+  HIREZ = 'hirez_public_apis'
   STATUS_PAGE = 'https://stk4xr7r1y0r.statuspage.io'
   STATUS_PAGE = 'https://status.hirezstudios.com'
 
