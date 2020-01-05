32549da93ed6f2ff80c728ee75df22f7c2401049
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2020-01-04 21:09:36 -0300

    Update player_not_found.py

--- pyrez/exceptions/player_not_found.py
+++ pyrez/exceptions/player_not_found.py
@@ -5,6 +5,7 @@
 
 from .unknown_player import UnknownPlayer
 class PlayerNotFound(UnknownPlayer):
+  """Raised when a player isn't found with specified name and platform"""
   def __init__(self, *args, **kw):
     super().__init__(*args, **kw)
 
