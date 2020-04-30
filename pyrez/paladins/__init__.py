ce40ab5dce19066e0cd2aba4d9fea0ef47b6a405
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2020-04-30 11:04:45 -0300

    Fix imports

--- pyrez/paladins/__init__.py
+++ pyrez/paladins/__init__.py
@@ -4,10 +4,14 @@
 # -*- coding: utf-8 -*-
 
 from ..base.paladins_smite import PaladinsSmite
-from ..utils.cache import cache
 from ..enums.language import Language
+from .player import Player
+from .player.champion import Champion
+from .player.loadout import Loadout
+from ..utils.cache import cache
 
 class Paladins(PaladinsSmite):
+'''
   # GET /getchampions[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{language_code}
   def champions(self, language=Language.English):
     """Returns a list of Champion objects containing all the champions and details about them."""
@@ -23,7 +27,7 @@ class Paladins(PaladinsSmite):
   def champion_skins(self, god_id, language=Language.English):
     """Returns all available skins for a particular Champion."""
     return self.request('getchampionskins', params=[god_id, language or Language.English])
-  '''
+'''
 
   @cache.defaults('getchampioncards', timeout=720)
   def god_cards(self, god_id, language=None, **kw):
@@ -32,25 +36,20 @@ class Paladins(PaladinsSmite):
     Returns a list of all the cards available for chosen champion and details about them.
     """
     #champion_
-    from ..enums.champion import Champion
-    from ..enums.language import Language
     if kw.pop('full', None):
       return self.items(Language(language), filter_by=kw.pop('filter_by', 'champion_id'), accepted_values=kw.pop('accepted_values', [Champion(god_id) or god_id]), **kw)
     return self.request('getchampioncards', params=[Champion(god_id) or god_id, Language(language)])
 
   def items(self, language=None, **kw):
     """Returns all the items in the game, including cards, items etc…"""
-    from ..enums.language import Language
     return super().items(Language(language), filter_by=kw.pop('filter_by', 'champion_id'), accepted_values=kw.pop('accepted_values', [0]), **kw)
 
   def player(self, player, portal_id=None, **kw):
-    from .player import Player
     return super().player(player, portal_id=portal_id, cls=kw.pop('cls', Player), **kw)
 
   @cache.defaults('getchampioncards', timeout=30)
   def player_gods(self, player_id, **kw):
     """GET /getplayerchampions[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{player_id}"""
-    from .player.champion import Champion
     return self.request('getplayerchampions', params=player_id, cls=kw.pop('cls', Champion), **kw)
 
   @cache.defaults('getchampioncards', timeout=30)
@@ -59,15 +58,11 @@ class Paladins(PaladinsSmite):
 
     Returns champion loadouts for player.
     """
-    from ..enums.language import Language
-    from .player.loadout import Loadout
     return self.request('getplayerloadouts', params=[player_id, Language(language)], cls=kw.pop('cls', Loadout), **kw)
 
   def players_from_match(self, match_id, **kw):
     """GET /getplayerbatchfrommatch[response_format]/{dev_id}/{signature}/{session_id}/{timestamp}/{match_id}"""
-    from .player import Player
     return self.request('getplayerbatchfrommatch', params=match_id, cls=kw.pop('cls', Player), sorted_by=kw.pop('sorted_by', 'ActivePlayerId'), **kw)
-'''
 
 __all__ = (
   'Paladins',
